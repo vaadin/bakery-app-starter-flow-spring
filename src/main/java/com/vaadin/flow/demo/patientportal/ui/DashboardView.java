4e53f50681f04cc88a8e40460e36a7af6dac2e7e
Diego Cardoso <diegopc86@gmail.com>
2017-07-10 11:46:32 +0300

    Add Yearly sales chart to dashboard (#11)

--- src/main/java/com/vaadin/flow/demo/patientportal/ui/DashboardView.java
+++ src/main/java/com/vaadin/flow/demo/patientportal/ui/DashboardView.java
@@ -1,10 +1,16 @@
 package com.vaadin.flow.demo.patientportal.ui;
 
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.vaadin.annotations.HtmlImport;
 import com.vaadin.annotations.Tag;
+import com.vaadin.flow.demo.patientportal.backend.data.DashboardData;
 import com.vaadin.flow.demo.patientportal.ui.dataproviders.OrdersDataProvider;
+import com.vaadin.flow.demo.patientportal.ui.entities.NamedSeries;
 import com.vaadin.flow.demo.patientportal.ui.entities.Order;
 import com.vaadin.flow.router.View;
 import com.vaadin.flow.template.PolymerTemplate;
@@ -15,7 +21,11 @@ import com.vaadin.flow.template.model.TemplateModel;
 public class DashboardView extends PolymerTemplate<DashboardView.Model> implements View {
 
 	public DashboardView() {
-		getElement().addAttachListener(e -> populateOrdersList(0, 10));
+		getElement().addAttachListener(e -> {
+			populateOrdersList(0, 10);
+			DashboardData data = OrdersDataProvider.get().getDashboardData();
+			populateYearlySalesChart(data);
+		});
 	}
 
 	private void populateOrdersList(int start, int count) {
@@ -24,7 +34,29 @@ public class DashboardView extends PolymerTemplate<DashboardView.Model> implemen
 		getModel().setOrders(ordersList);
 	}
 
+	private void populateYearlySalesChart(DashboardData data) {
+		List<NamedSeries> yearlySales = new ArrayList<>();
+
+		int year = Year.now().getValue();
+
+		for (int i = 0; i < 3; i++) {
+			NamedSeries series = new NamedSeries();
+
+			series.setSeries(Arrays.asList(data.getSalesPerMonth(i))
+					.stream()
+					.filter(number -> number != null)
+					.map(Number::toString).collect(Collectors.toList()));
+
+			series.setTitle(Integer.toString(year - i));
+			yearlySales.add(series);
+		}
+		getModel().setSalesGraphSeries(yearlySales);
+		getModel().setSalesGraphTitle("Sales last years");
+	}
+
 	public interface Model extends TemplateModel {
 		void setOrders(List<Order> orders);
+		void setSalesGraphSeries(List<NamedSeries> sales);
+		void setSalesGraphTitle(String title);
 	}
 }
