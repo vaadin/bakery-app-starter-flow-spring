04342883b72007257f23cee618a73446006d32a6
rogozinds <rogozinds@gmail.com>
2017-07-10 10:55:24 +0300

    Add access control for Views via using @Secured BFF-76

--- src/main/java/com/vaadin/flow/demo/patientportal/ui/UsersView.java
+++ src/main/java/com/vaadin/flow/demo/patientportal/ui/UsersView.java
@@ -19,9 +19,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 
 import com.vaadin.annotations.HtmlImport;
 import com.vaadin.annotations.Tag;
+import com.vaadin.flow.demo.patientportal.backend.data.Role;
 import com.vaadin.flow.demo.patientportal.backend.service.UserService;
 import com.vaadin.flow.demo.patientportal.ui.utils.BakeryConst;
 import com.vaadin.flow.router.View;
@@ -33,87 +35,86 @@ import com.vaadin.ui.AttachEvent;
 
 /**
  * @author Vaadin Ltd
- *
  */
 @Tag("bakery-users")
 @HtmlImport("frontend://src/users/bakery-users.html")
 @Route(BakeryConst.PAGE_USERS)
 @ParentView(BakeryApp.class)
-// @Secured(Role.ADMIN) Secured annotation does not work.
+@Secured(Role.ADMIN)
 public class UsersView extends PolymerTemplate<UsersView.Model> implements View {
 
-	public static class UserModel {
-		private String name;
-		private String last;
-		private String email;
-		private String picture;
-		private String role;
+    public static class UserModel {
+        private String name;
+        private String last;
+        private String email;
+        private String picture;
+        private String role;
 
-		public String getName() {
-			return name;
-		}
+        public String getName() {
+            return name;
+        }
 
-		public void setName(String name) {
-			this.name = name;
-		}
+        public void setName(String name) {
+            this.name = name;
+        }
 
-		public String getLast() {
-			return last;
-		}
+        public String getLast() {
+            return last;
+        }
 
-		public void setLast(String last) {
-			this.last = last;
-		}
+        public void setLast(String last) {
+            this.last = last;
+        }
 
-		public String getEmail() {
-			return email;
-		}
+        public String getEmail() {
+            return email;
+        }
 
-		public void setEmail(String email) {
-			this.email = email;
-		}
+        public void setEmail(String email) {
+            this.email = email;
+        }
 
-		public String getPicture() {
-			return picture;
-		}
+        public String getPicture() {
+            return picture;
+        }
 
-		public void setPicture(String picture) {
-			this.picture = picture;
-		}
+        public void setPicture(String picture) {
+            this.picture = picture;
+        }
 
-		public String getRole() {
-			return role;
-		}
+        public String getRole() {
+            return role;
+        }
 
-		public void setRole(String role) {
-			this.role = role;
-		}
-	}
+        public void setRole(String role) {
+            this.role = role;
+        }
+    }
 
-	public interface Model extends TemplateModel {
-		void setUsers(List<UserModel> users);
-	}
+    public interface Model extends TemplateModel {
+        void setUsers(List<UserModel> users);
+    }
 
-	private UserService userService;
+    private UserService userService;
 
-	@Autowired
-	public UsersView(UserService userService) {
-		this.userService = userService;
-	}
+    @Autowired
+    public UsersView(UserService userService) {
+        this.userService = userService;
+    }
 
-	@Override
-	protected void onAttach(AttachEvent event) {
-		super.onAttach(event);
+    @Override
+    protected void onAttach(AttachEvent event) {
+        super.onAttach(event);
 
-		List<UserModel> users = userService.getRepository().findAll().stream().map(user -> {
-			UserModel model = new UserModel();
-			model.setName(user.getName());
-			model.setLast("McLast");
-			model.setEmail(user.getEmail());
-			model.setPicture("https://randomuser.me/api/portraits/women/10.jpg");
-			model.setRole(user.getRole());
-			return model;
-		}).collect(Collectors.toList());
-		getModel().setUsers(users);
-	}
+        List<UserModel> users = userService.getRepository().findAll().stream().map(user -> {
+            UserModel model = new UserModel();
+            model.setName(user.getName());
+            model.setLast("McLast");
+            model.setEmail(user.getEmail());
+            model.setPicture("https://randomuser.me/api/portraits/women/10.jpg");
+            model.setRole(user.getRole());
+            return model;
+        }).collect(Collectors.toList());
+        getModel().setUsers(users);
+    }
 }
