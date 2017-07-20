421d47ee7103e5d18ef3a6f1700338d6bfdfe6bf
Alex <alex@vaadin.com>
2017-07-20 18:00:05 +0300

    DashboardView: Merged with lazy loading

--- src/main/java/com/vaadin/starter/bakery/ui/DashboardView.java
+++ src/main/java/com/vaadin/starter/bakery/ui/DashboardView.java
@@ -7,6 +7,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+
 import com.google.gson.Gson;
 import com.vaadin.annotations.EventData;
 import com.vaadin.annotations.EventHandler;
@@ -31,7 +34,6 @@ import com.vaadin.starter.bakery.ui.utils.DashboardUtils.OrdersCountData;
 import com.vaadin.starter.bakery.ui.utils.DashboardUtils.OrdersCountDataWithChart;
 import com.vaadin.starter.bakery.ui.utils.DashboardUtils.PageInfo;
 import com.vaadin.ui.AttachEvent;
-import org.springframework.data.domain.PageRequest;
 
 @Tag("bakery-dashboard")
 @HtmlImport("frontend://src/dashboard/bakery-dashboard.html")
@@ -49,7 +51,7 @@ public class DashboardView extends PolymerTemplate<DashboardView.Model> implemen
 	@Override
 	protected void onAttach(AttachEvent attachEvent) {
 		populateOrdersCount();
-		DashboardData data = OrdersDataProvider.get().getDashboardData();
+		DashboardData data = ordersProvider.getDashboardData();
 		populateYearlySalesChart(data);
 		populateDeliveriesCharts(data);
 		populateOrdersCounts(data.getDeliveryStats());
@@ -71,18 +73,17 @@ public class DashboardView extends PolymerTemplate<DashboardView.Model> implemen
 	}
 
 	private void populateOrdersCount() {
-		getModel().setOrdersCount((int) OrdersDataProvider.get().countAnyMatchingAfterDueDate());
+		getModel().setOrdersCount((int) ordersProvider.countAnyMatchingAfterDueDate());
 	}
 
 	@EventHandler
 	private void loadOrdersPage(@EventData("event.detail.page") int page,
-								@EventData("event.detail.pageSize") int pageSize) {
-		PageInfo pageInfo = OrdersDataProvider.get().getOrdersList(new PageRequest(page, pageSize));
-		//It should be easier to use a complex object as function argument
+			@EventData("event.detail.pageSize") int pageSize) {
+		PageInfo pageInfo = ordersProvider.getOrdersList(new PageRequest(page, pageSize));
+		// It should be easier to use a complex object as function argument
 		String json = new Gson().toJson(pageInfo);
 		getElement().callFunction("loadPage", json);
 	}
-
 
 	private void populateDeliveriesCharts(DashboardData data) {
 		ColumnChartData deliveriesThisMonth = DashboardUtils
