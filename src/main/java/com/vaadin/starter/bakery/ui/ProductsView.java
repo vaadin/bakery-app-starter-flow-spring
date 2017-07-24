c668eb718aa3f2f6ab3d733f4ee168bb15016d7a
Diego Cardoso <diego@vaadin.com>
2017-07-24 16:02:02 +0300

    Add feature to create/edit Product

--- src/main/java/com/vaadin/starter/bakery/ui/ProductsView.java
+++ src/main/java/com/vaadin/starter/bakery/ui/ProductsView.java
@@ -1,14 +1,14 @@
 package com.vaadin.starter.bakery.ui;
 
-import com.vaadin.annotations.ClientDelegate;
+import com.vaadin.annotations.*;
+import com.vaadin.starter.bakery.app.HasLogger;
 import com.vaadin.starter.bakery.ui.dataproviders.ProductsDataProvider;
 import com.vaadin.starter.bakery.ui.entities.Product;
 import com.vaadin.ui.AttachEvent;
+import elemental.json.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 
-import com.vaadin.annotations.HtmlImport;
-import com.vaadin.annotations.Tag;
 import com.vaadin.flow.router.View;
 import com.vaadin.flow.template.PolymerTemplate;
 import com.vaadin.flow.template.model.TemplateModel;
@@ -17,15 +17,15 @@ import com.vaadin.hummingbird.ext.spring.annotations.Route;
 import com.vaadin.starter.bakery.backend.data.Role;
 import com.vaadin.starter.bakery.ui.utils.BakeryConst;
 
+import javax.xml.bind.ValidationException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Tag("bakery-products")
 @HtmlImport("frontend://src/products/bakery-products.html")
 @Route(BakeryConst.PAGE_PRODUCTS)
 @ParentView(BakeryApp.class)
 @Secured(Role.ADMIN)
-public class ProductsView extends PolymerTemplate<ProductsView.Model> implements View {
+public class ProductsView extends PolymerTemplate<ProductsView.Model> implements View, HasLogger {
 
 	public interface Model extends TemplateModel {
 		void setProducts(List<Product> products);
@@ -47,5 +47,20 @@ public class ProductsView extends PolymerTemplate<ProductsView.Model> implements
 	@ClientDelegate
 	public void onFilterProducts(String filterValue) {
 		getModel().setProducts(productsDataProvider.findByName(filterValue));
+	}
+
+	@ClientDelegate
+	public void saveProduct(JsonObject product){
+		try {
+			productsDataProvider.save(product);
+			getModel().setProducts(productsDataProvider.findAll());
+			getElement().callFunction("editProduct");
+		} catch (ValidationException e) {
+			getElement().callFunction("showErrorMessage", e.getMessage());
+			getLogger().error("Error on saving product: " + e.getMessage());
+		} catch (Exception e) {
+			getElement().callFunction("showErrorMessage", "Product could not be saved.");
+			getLogger().error("Error on saving product: " + e.getMessage());
+		}
 	}
 }
--- src/main/java/com/vaadin/starter/bakery/ui/dataproviders/ProductsDataProvider.java
+++ src/main/java/com/vaadin/starter/bakery/ui/dataproviders/ProductsDataProvider.java
@@ -1,10 +1,13 @@
 package com.vaadin.starter.bakery.ui.dataproviders;
 
+import com.google.gson.Gson;
 import com.vaadin.starter.bakery.backend.service.ProductService;
 import com.vaadin.starter.bakery.ui.entities.Product;
+import elemental.json.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.xml.bind.ValidationException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,9 +33,23 @@ public class ProductsDataProvider {
 		}
 		String lowerName = name.toLowerCase();
 		return productService.getRepository().findAll().stream()
-				.filter(product -> product.getName().toLowerCase().contains(lowerName))
+				.filter(product -> product.getName() != null && product.getName().toLowerCase().contains(lowerName))
 				.map(this::toUiEntity)
 				.collect(Collectors.toList());
+	}
+
+	public void save(JsonObject product) throws ValidationException {
+		com.vaadin.starter.bakery.backend.data.entity.Product productToSave = toDataEntity(product);
+
+		if (productToSave.getName() == null || productToSave.getName().trim().length() == 0) {
+			throw new ValidationException("Name is required.");
+		}
+
+		if (productToSave.getPrice() <= 0) {
+			throw new ValidationException("Price is required and must be higher than zero.");
+		}
+
+		productService.save(productToSave);
 	}
 
 	private Product toUiEntity(com.vaadin.starter.bakery.backend.data.entity.Product product) {
@@ -43,5 +60,30 @@ public class ProductsDataProvider {
 		uiEntity.setPrice(product.getPrice());
 
 		return uiEntity;
+	}
+
+	private com.vaadin.starter.bakery.backend.data.entity.Product toDataEntity(
+			JsonObject user) {
+		Gson gson = new Gson();
+		Product uiEntity = gson.fromJson(user.toJson(), Product.class);
+		return toDataEntity(uiEntity);
+	}
+
+	private com.vaadin.starter.bakery.backend.data.entity.Product toDataEntity(Product product) {
+		com.vaadin.starter.bakery.backend.data.entity.Product dataEntity = null;
+
+		if (product.getId() != null) {
+			long id = Long.parseLong(product.getId());
+			dataEntity = productService.load(id);
+		}
+
+		if (dataEntity == null) {
+			dataEntity = new com.vaadin.starter.bakery.backend.data.entity.Product();
+		}
+
+		dataEntity.setName(product.getName());
+		dataEntity.setPrice(product.getPrice());
+
+		return dataEntity;
 	}
 }
