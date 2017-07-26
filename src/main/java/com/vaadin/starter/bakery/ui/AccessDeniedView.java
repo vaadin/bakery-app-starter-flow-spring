0cd15b5dfa636bc7634e70d1a45afeb1900593e1
Viktor Lukashov <viktor@vaadin.com>
2017-07-26 05:22:11 +0300

    Add correct HTTP status codes for Not Found (404) and Forbidden (403) pages

--- src/main/java/com/vaadin/starter/bakery/ui/AccessDeniedView.java
+++ src/main/java/com/vaadin/starter/bakery/ui/AccessDeniedView.java
@@ -17,6 +17,7 @@ package com.vaadin.starter.bakery.ui;
 
 import com.vaadin.annotations.HtmlImport;
 import com.vaadin.annotations.Tag;
+import com.vaadin.flow.router.LocationChangeEvent;
 import com.vaadin.flow.router.View;
 import com.vaadin.flow.template.PolymerTemplate;
 import com.vaadin.flow.template.model.TemplateModel;
@@ -24,12 +25,13 @@ import com.vaadin.hummingbird.ext.spring.annotations.ParentView;
 import com.vaadin.hummingbird.ext.spring.annotations.Route;
 import com.vaadin.starter.bakery.ui.utils.BakeryConst;
 
-/**
- * @author Vaadin Ltd
- */
 @Tag("access-denied")
 @HtmlImport("frontend://src/admin/access-denied.html")
 @ParentView(BakeryApp.class)
 @Route(BakeryConst.ACCESS_DENIED)
 public class AccessDeniedView extends PolymerTemplate<TemplateModel> implements View {
+	@Override
+	public void onLocationChange(LocationChangeEvent locationChangeEvent) {
+		locationChangeEvent.setStatusCode(403);
+	}
 }
--- src/main/java/com/vaadin/starter/bakery/ui/NotFoundView.java
+++ src/main/java/com/vaadin/starter/bakery/ui/NotFoundView.java
@@ -2,6 +2,7 @@ package com.vaadin.starter.bakery.ui;
 
 import com.vaadin.annotations.HtmlImport;
 import com.vaadin.annotations.Tag;
+import com.vaadin.flow.router.LocationChangeEvent;
 import com.vaadin.flow.router.View;
 import com.vaadin.flow.template.PolymerTemplate;
 import com.vaadin.flow.template.model.TemplateModel;
@@ -14,5 +15,8 @@ import com.vaadin.starter.bakery.ui.utils.BakeryConst;
 @Route(value = BakeryConst.PAGE_NOTFOUND)
 @ParentView(BakeryApp.class)
 public class NotFoundView extends PolymerTemplate<TemplateModel> implements View {
-
+	@Override
+	public void onLocationChange(LocationChangeEvent locationChangeEvent) {
+		locationChangeEvent.setStatusCode(404);
+	}
 }
