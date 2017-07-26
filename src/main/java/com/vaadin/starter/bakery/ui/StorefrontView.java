d7f8ae5d98fc60b636425bb83dd64e5710ef3d62
Tulio Garcia <tulio@vaadin.com>
2017-07-26 15:09:35 +0300

    Implemented admin access to all the views in the system (#BFF-75)

--- src/main/java/com/vaadin/starter/bakery/ui/StorefrontView.java
+++ src/main/java/com/vaadin/starter/bakery/ui/StorefrontView.java
@@ -21,6 +21,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.security.access.annotation.Secured;
 
 import com.vaadin.annotations.ClientDelegate;
 import com.vaadin.annotations.HtmlImport;
@@ -30,6 +31,7 @@ import com.vaadin.flow.template.PolymerTemplate;
 import com.vaadin.flow.template.model.TemplateModel;
 import com.vaadin.hummingbird.ext.spring.annotations.ParentView;
 import com.vaadin.hummingbird.ext.spring.annotations.Route;
+import com.vaadin.starter.bakery.backend.data.Role;
 import com.vaadin.starter.bakery.ui.dataproviders.OrdersDataProvider;
 import com.vaadin.starter.bakery.ui.dataproviders.ProductsDataProvider;
 import com.vaadin.starter.bakery.ui.entities.Order;
@@ -48,6 +50,7 @@ import elemental.json.JsonObject;
 @Route(BakeryConst.PAGE_STOREFRONT)
 @Route(value = "")
 @ParentView(BakeryApp.class)
+@Secured(Role.BARISTA)
 public class StorefrontView extends PolymerTemplate<StorefrontView.Model> implements View {
 
 	public interface Model extends TemplateModel {
