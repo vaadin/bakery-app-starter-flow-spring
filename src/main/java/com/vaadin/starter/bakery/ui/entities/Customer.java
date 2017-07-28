776b107817a62225b989aff2acf038fb3eaf94d2
Alex <alex@vaadin.com>
2017-07-28 11:10:14 +0300

    Implemented loading customer list from repository

--- src/main/java/com/vaadin/starter/bakery/ui/entities/Customer.java
+++ src/main/java/com/vaadin/starter/bakery/ui/entities/Customer.java
@@ -8,7 +8,7 @@ public class Customer implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Customer [name=" + name + "]";
+		return "Customer [name=" + name + ", number=" + number + ", details=" + details + "]";
 	}
 
 	private String name;
@@ -47,6 +47,43 @@ public class Customer implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((details == null) ? 0 : details.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((number == null) ? 0 : number.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (details == null) {
+			if (other.details != null)
+				return false;
+		} else if (!details.equals(other.details))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (number == null) {
+			if (other.number != null)
+				return false;
+		} else if (!number.equals(other.number))
+			return false;
+		return true;
 	}
 
 }
