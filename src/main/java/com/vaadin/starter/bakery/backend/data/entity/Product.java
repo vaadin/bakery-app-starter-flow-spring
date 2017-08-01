e0326e25ddb00e23ec7aad5fef1cd7b0ce1df0c5
tuliogarcia <tulio@vaadin.com>
2017-08-01 13:20:09 +0300

    Avoid adding unnamed products - BFF-24 (#58)
    * Implemented #BFF-52
    
    As a user I want that order cannot be placed without customer name and
    contact details
    
    On the backend, added @NotBlank to Customer name and phone number.
    
    On the frontend, added the required property to the respective fields.
    Also added the iron-form, so when the users clicks review it validates
    the data and either proceeds or warns the user to fill out all required
    fields.
    
    Had to add dependency to iron-form on bower.json
    
    * Remove redundant annotation @NotNull and constructor.
    
    add validation to html
    
    * Remove unused import
    
    * Implemented frontend validation of field.

--- src/main/java/com/vaadin/starter/bakery/backend/data/entity/Product.java
+++ src/main/java/com/vaadin/starter/bakery/backend/data/entity/Product.java
@@ -1,27 +1,21 @@
 package com.vaadin.starter.bakery.backend.data.entity;
 
-import org.hibernate.validator.constraints.NotBlank;
-
 import javax.persistence.Entity;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
 public class Product extends AbstractEntity {
 
 	@Size(max = 255, message = "The maximum length of a product name is 255 characters.")
-	@NotNull(message = "Name is required.")
 	@NotBlank(message = "Name is required.")
 	private String name;
 
 	// Real price * 100 as an int to avoid rounding errors
 	@Min(value = 0, message = "Price must be equal or higher than zero.")
 	private int price;
-
-	public Product() {
-		// Empty constructor is needed by Spring Data / JPA
-	}
 
 	public String getName() {
 		return name;
