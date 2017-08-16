8868aa5baf8d93d63f2618c425fa287d18611557
Tulio Garcia <tulio@vaadin.com>
2017-08-16 18:05:25 +0300

    Fix polymer.json and LoginView

--- src/main/java/com/vaadin/starter/bakery/ui/LoginView.java
+++ src/main/java/com/vaadin/starter/bakery/ui/LoginView.java
@@ -2,28 +2,27 @@ package com.vaadin.starter.bakery.ui;
 
 import com.vaadin.annotations.HtmlImport;
 import com.vaadin.annotations.Tag;
+import com.vaadin.annotations.Title;
 import com.vaadin.flow.router.LocationChangeEvent;
 import com.vaadin.flow.router.View;
 import com.vaadin.flow.template.PolymerTemplate;
 import com.vaadin.flow.template.model.TemplateModel;
-import com.vaadin.hummingbird.ext.spring.annotations.ParentView;
 import com.vaadin.hummingbird.ext.spring.annotations.Route;
-import com.vaadin.starter.bakery.ui.utils.BakeryConst;
 
 @Tag("bakery-login")
 @HtmlImport("frontend://src/login/bakery-login.html")
 @Route("login")
-public class LoginView extends PolymerTemplate<TemplateModel> implements View{
+@Title("###Bakery###")
+public class LoginView extends PolymerTemplate<LoginView.Model> implements View {
 
 	@Override
 	public void onLocationChange(LocationChangeEvent locationChangeEvent) {
 		boolean error = locationChangeEvent.getQueryParameters().containsKey("error");
-		getElement().setProperty("error", error);
+		getModel().setError(error);
 	}
 
-	@Override
-	public String getTitle(LocationChangeEvent locationChangeEvent) {
-		return "###Bakery###";
+	public interface Model extends TemplateModel {
+		void setError(boolean error);
 	}
 
 }
