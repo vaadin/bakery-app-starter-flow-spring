ddb85bc409a5f89deb3b668fc9dd6b4344734323
Guillermo Alvarez <guillermo@vaadin.com>
2017-08-22 17:25:12 +0300

    Fix linter for login and serviceworker

--- src/main/webapp/service-worker.js
+++ src/main/webapp/service-worker.js
@@ -15,26 +15,26 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
-    );
+  );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-    .then(function (response) {
-      if (response) {
-        console.log('Found response in cache:', response);
-        return response;
-      }
-      console.log('No response found in cache. About to fetch from network...');
-      return fetch(event.request)
-        .then(function (response) {
-          console.log('Response from network is:', response);
+      .then(function(response) {
+        if (response) {
+          window.console.log('Found response in cache:', response);
           return response;
-        })
-    })
-    .catch(function (error) {
-      console.error('Fetching failed:', error);
-    })
+        }
+        window.console.log('No response found in cache. About to fetch from network...');
+        return fetch(event.request)
+          .then(function(response) {
+            window.console.log('Response from network is:', response);
+            return response;
+          });
+      })
+      .catch(function(error) {
+        window.console.error('Fetching failed:', error);
+      })
   );
 });
