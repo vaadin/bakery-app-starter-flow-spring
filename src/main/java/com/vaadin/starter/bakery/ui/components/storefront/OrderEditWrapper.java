b46825d4d3bb338e4a331fa02a279772dd51a306
Tulio Garcia <tulio@vaadin.com>
2017-09-08 17:41:50 +0300

    Moving form to order-edit

---
+++ src/main/java/com/vaadin/starter/bakery/ui/components/storefront/OrderEditWrapper.java
@@ -0,0 +1,11 @@
+package com.vaadin.starter.bakery.ui.components.storefront;
+
+import com.vaadin.annotations.HtmlImport;
+import com.vaadin.annotations.Tag;
+import com.vaadin.flow.template.PolymerTemplate;
+import com.vaadin.flow.template.model.TemplateModel;
+@Tag("order-edit-wrapper")
+@HtmlImport("frontend://src/storefront/order-edit-wrapper.html")
+public class OrderEditWrapper extends PolymerTemplate<TemplateModel> {
+
+}
