d3a82db662df0118958495c6b88cd37418236e9d
Guillermo Alvarez <guillermo@vaadin.com>
2017-09-08 16:38:21 +0300

    Remove user avatar java template
    It was causing issues in prod mode, now the avatar src is handled via model

--- src/main/java/com/vaadin/starter/bakery/ui/components/UserEdit.java
+++ src/main/java/com/vaadin/starter/bakery/ui/components/UserEdit.java
@@ -13,7 +13,6 @@ import com.vaadin.flow.template.model.TemplateModel;
 import com.vaadin.shared.Registration;
 import com.vaadin.starter.bakery.backend.data.Role;
 import com.vaadin.starter.bakery.backend.data.entity.User;
-import com.vaadin.ui.AttachEvent;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.HasClickListeners.ClickEvent;
@@ -25,14 +24,8 @@ import com.vaadin.ui.TextField;
 public class UserEdit extends PolymerTemplate<UserEdit.Model> implements View {
 
 	public interface Model extends TemplateModel {
-		void setShowAvatar(boolean showAvatar);
+		void setAvatar(String avatar);
 	}
-
-	// A workaround for a Flow issue (see BFF-244 for details).
-	// Instead of adding a slot into the template, and then populating it with a <user-avatar>
-	// element created from the server-side at run time, <user-avatar> would be placed
-	// directly into the template and referenced with @Id from the server-side.
-	private UserAvatar avatar;
 
 	@Id("title")
 	private H3 title;
@@ -65,10 +58,6 @@ public class UserEdit extends PolymerTemplate<UserEdit.Model> implements View {
 	private Binder<User> binder = new Binder<>();
 
 	public UserEdit() {
-		avatar = new UserAvatar();
-		avatar.getElement().setAttribute("slot", "avatar");
-		getElement().appendChild(avatar.getElement());
-
 		roleField.setItems(Role.getAllRoles());
 
 		binder.bind(firstnameField, User::getFirstName, User::setFirstName);
@@ -103,8 +92,7 @@ public class UserEdit extends PolymerTemplate<UserEdit.Model> implements View {
 		binder.readBean(user);
 
 		title.setText((user.isNew() ? "New" : "Edit") + " User");
-		avatar.setSrc(user.getPhotoUrl());
-		getModel().setShowAvatar(user.getPhotoUrl() != null);
+		getModel().setAvatar(user.getPhotoUrl());
 		saveButton.setDisabled(true);
 		deleteButton.setDisabled(user.isNew());
 	}
