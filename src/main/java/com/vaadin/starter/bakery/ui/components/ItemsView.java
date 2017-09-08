2775524c972c5401a3e0ee3113df2cdde7ff0010
Guille <alvarezguille@users.noreply.github.com>
2017-09-08 13:41:23 +0300

    Revert "BFF-103: use Java as much as possible when developing the Users view (#111)" (#119)
    This reverts commit 10ba8d9fd31095704e8b8105645b9ba639e6f2ec.

--- src/main/java/com/vaadin/starter/bakery/ui/components/ItemsView.java
+++ src/main/java/com/vaadin/starter/bakery/ui/components/ItemsView.java
@@ -2,10 +2,8 @@ package com.vaadin.starter.bakery.ui.components;
 
 import com.vaadin.annotations.HtmlImport;
 import com.vaadin.annotations.Tag;
-import com.vaadin.flow.dom.DomEventListener;
 import com.vaadin.flow.template.PolymerTemplate;
 import com.vaadin.flow.template.model.TemplateModel;
-import com.vaadin.shared.Registration;
 
 @Tag("items-view")
 @HtmlImport("frontend://src/elements/items-view.html")
@@ -17,9 +15,5 @@ public class ItemsView extends PolymerTemplate<ItemsView.Model> {
 
 	public void openDialog(boolean open) {
 		getModel().setEditing(open);
-	}
-
-	public Registration addNewListener(DomEventListener listener) {
-		return getElement().addEventListener("new-item", listener);
 	}
 }
