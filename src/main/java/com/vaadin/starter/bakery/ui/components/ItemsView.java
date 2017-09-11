fe349dc62b708befcd3e8c385655c2d351115f97
Sayo Oladeji <sayo@vaadin.com>
2017-09-11 14:20:21 +0300

    Moved search to server-side

--- src/main/java/com/vaadin/starter/bakery/ui/components/ItemsView.java
+++ src/main/java/com/vaadin/starter/bakery/ui/components/ItemsView.java
@@ -1,11 +1,18 @@
 package com.vaadin.starter.bakery.ui.components;
 
 import com.vaadin.annotations.HtmlImport;
+import com.vaadin.annotations.Id;
 import com.vaadin.annotations.Tag;
 import com.vaadin.flow.dom.DomEventListener;
 import com.vaadin.flow.template.PolymerTemplate;
 import com.vaadin.flow.template.model.TemplateModel;
 import com.vaadin.shared.Registration;
+import com.vaadin.flow.event.ComponentEventListener;
+import com.vaadin.starter.bakery.ui.BakerySearch;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HasClickListeners;
+
+import java.util.function.Consumer;
 
 @Tag("items-view")
 @HtmlImport("frontend://src/elements/items-view.html")
@@ -15,11 +22,30 @@ public class ItemsView extends PolymerTemplate<ItemsView.Model> {
 		void setEditing(Boolean editing);
 	}
 
-	public void openDialog(boolean open) {
-		getModel().setEditing(open);
+	@Id("item-search")
+	private BakerySearch searchBar;
+
+	public ItemsView() {
+		searchBar.setPlaceHolder("Search");
 	}
 
-	public Registration addNewListener(DomEventListener listener) {
-		return getElement().addEventListener("new-item", listener);
+	public void addFilterChangeListener(Consumer<String> consumer) {
+		searchBar.addFilterChangeListener((filter, showPrevious) -> consumer.accept(filter));
+	}
+
+	public void addActionClickListener(ComponentEventListener<HasClickListeners.ClickEvent<Button>> listener) {
+		searchBar.addActionClickListener(listener);
+	}
+
+	public void setActionText(String actionText) {
+		searchBar.setActionText(actionText);
+	}
+
+	public String getFilter() {
+		return searchBar.getFilter();
+	}
+
+	public void openDialog(boolean open) {
+		getModel().setEditing(open);
 	}
 }
