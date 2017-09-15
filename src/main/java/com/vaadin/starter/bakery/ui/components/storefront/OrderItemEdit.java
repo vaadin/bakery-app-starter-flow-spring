fceb53e3dd6f6dd3fd4ee2e459e400f125eedc63
Tulio Garcia <tulio@vaadin.com>
2017-09-15 16:27:53 +0300

    Fixing product selection
    (BFF-99)

--- src/main/java/com/vaadin/starter/bakery/ui/components/storefront/OrderItemEdit.java
+++ src/main/java/com/vaadin/starter/bakery/ui/components/storefront/OrderItemEdit.java
@@ -8,19 +8,21 @@ import com.vaadin.annotations.Id;
 import com.vaadin.annotations.Tag;
 import com.vaadin.components.data.HasValue;
 import com.vaadin.data.BeanValidationBinder;
+import com.vaadin.flow.event.ComponentEventListener;
 import com.vaadin.flow.html.Div;
 import com.vaadin.flow.template.PolymerTemplate;
 import com.vaadin.flow.template.model.TemplateModel;
+import com.vaadin.shared.Registration;
 import com.vaadin.starter.bakery.backend.data.entity.OrderItem;
 import com.vaadin.starter.bakery.backend.data.entity.Product;
 import com.vaadin.starter.bakery.ui.components.AmountField;
 import com.vaadin.starter.bakery.ui.utils.FormattingUtils;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.ComponentEvent;
 import com.vaadin.ui.TextField;
 
 /**
- * @author tulio
  *
  */
 @Tag("order-item-edit")
@@ -48,30 +50,27 @@ public class OrderItemEdit extends PolymerTemplate<TemplateModel> implements Has
 
 	private ProductSource productSource;
 
-
 	public OrderItemEdit(OrderItemsEdit parent, ProductSource productSource) {
 
 		this.productSource = productSource;
 		this.amount.setDisabled(true);
-	    productSource.setupBeanComboBox(products);
+		productSource.setupBeanComboBox(products);
 		products.addChangeListener(e -> {
+			fireEvent(new ProductChangeEvent(productSource.getProductByName(products.getValue())));
 			this.setPrice();
-			parent.productChanged(this,productSource.getProductByName(products.getValue()));
 			this.amount.setDisabled(false);
 			this.amount.setValue(1);
 		});
 
-		binder.forField(products).withConverter(productSource).bind("product");
+		// Bind with getter/setters to avoid required validation.
+		binder.forField(products).withConverter(productSource).bind(OrderItem::getProduct, OrderItem::setProduct);
 
 		binder.forField(amount).bind("quantity");
-		amount.addValueChangeListener(e -> { 
-			this.setPrice();
-			parent.priceChanged();
-		});
+		amount.addValueChangeListener(e -> this.setPrice());
 
 		binder.forField(comment).bind("comment");
-		
-		delete.addClickListener(e->parent.deleteItem(this));
+
+		delete.addClickListener(e -> fireEvent(new DeleteEvent()));
 		this.setPrice();
 	}
 
@@ -83,6 +82,7 @@ public class OrderItemEdit extends PolymerTemplate<TemplateModel> implements Has
 			totalPrice = selectedAmount * product.getPrice();
 		}
 		this.price.setText(FormattingUtils.formatAsCurrency(totalPrice));
+		fireEvent(new PriceChangeEvent(totalPrice));
 	}
 
 	@Override
@@ -105,4 +105,50 @@ public class OrderItemEdit extends PolymerTemplate<TemplateModel> implements Has
 		return this.orderItem;
 	}
 
+	public Registration addPriceChangeListener(ComponentEventListener<PriceChangeEvent> listener) {
+		return addListener(PriceChangeEvent.class, listener);
+	}
+
+	public Registration addProductChangeListener(ComponentEventListener<ProductChangeEvent> listener) {
+		return addListener(ProductChangeEvent.class, listener);
+	}
+	public Registration addDeleteListener(ComponentEventListener<DeleteEvent> listener) {
+		return addListener(DeleteEvent.class, listener);
+	}
+
+	public class PriceChangeEvent extends ComponentEvent<OrderItemEdit> {
+
+		private final Integer totalPrice;
+
+		PriceChangeEvent(Integer totalPrice) {
+			super(OrderItemEdit.this, false);
+			this.totalPrice = totalPrice;
+		}
+
+		public Integer getTotalPrice() {
+			return totalPrice;
+		}
+
+	}
+
+	public class ProductChangeEvent extends ComponentEvent<OrderItemEdit> {
+
+		private final Product product;
+
+		ProductChangeEvent(Product product) {
+			super(OrderItemEdit.this, false);
+			this.product = product;
+		}
+
+		public Product getProduct() {
+			return product;
+		}
+
+	}
+
+	public class DeleteEvent extends ComponentEvent<OrderItemEdit> {
+		DeleteEvent() {
+			super(OrderItemEdit.this, false);
+		}
+	}
 }
--- src/main/java/com/vaadin/starter/bakery/ui/components/storefront/OrderItemsEdit.java
+++ src/main/java/com/vaadin/starter/bakery/ui/components/storefront/OrderItemsEdit.java
@@ -19,31 +19,37 @@ import com.vaadin.starter.bakery.backend.data.entity.OrderItem;
 import com.vaadin.starter.bakery.backend.data.entity.Product;
 import com.vaadin.ui.ComponentEvent;
 
-/**
- * @author tulio
- *
- */
 @Tag("order-items-edit")
 @HtmlImport("frontend://src/storefront/order-items-edit.html")
-public class OrderItemsEdit extends PolymerTemplate<TemplateModel> implements HasValue<OrderItemsEdit, List<OrderItem>>  {
+public class OrderItemsEdit extends PolymerTemplate<OrderItemsEdit.Model>
+		implements HasValue<OrderItemsEdit, List<OrderItem>> {
 
 	private OrderItemEdit empty;
-	
+
 	private List<OrderItem> items;
-	
+
 	private List<OrderItemEdit> editors = new ArrayList<>(5);
-	
+
+	private List<Registration> registrations = new ArrayList<>(5);
+
 	private ProductSource productSource;
-	
-	
+
+	public interface Model extends TemplateModel {
+		void setTotalPrice(Integer total);
+
+		Integer getTotalPrice();
+	}
+
 	public void reset() {
+		registrations.forEach(Registration::remove);
+		registrations.clear();
 		editors.forEach(i -> getElement().removeChild(i.getElement()));
 		editors.clear();
-		if(items != null)
+		if (items != null)
 			items.clear();
 		getElement().removeAllChildren();
 	}
-	
+
 	void setProducts(Collection<Product> products) {
 		this.productSource = new ProductSource(products);
 	}
@@ -53,17 +59,46 @@ public class OrderItemsEdit extends PolymerTemplate<TemplateModel> implements Ha
 		reset();
 		this.items = items;
 
-		if(items != null) {
-			items.forEach(i -> {
-				OrderItemEdit editor = new OrderItemEdit(this, productSource);
-				editor.setValue(i);
-				editors.add(editor);
-				getElement().appendChild(editor.getElement());
-			});
+		if (items != null) {
+			items.forEach(this::createEditor);
 		}
+
 		createEmptyElement();
+
 	}
 
+	private OrderItemEdit createEditor(OrderItem value) {
+		OrderItemEdit editor = new OrderItemEdit(this, productSource);
+		editor.setValue(value);
+		editors.add(editor);
+		getElement().appendChild(editor.getElement());
+		Registration priceChangeRegistration = addRegistration(editor.addPriceChangeListener(e -> this.priceChanged()));
+		Registration productChangeRegistration = addRegistration(
+				editor.addProductChangeListener(e -> this.productChanged(e.getSource(), e.getProduct())));
+		editor.addDeleteListener(e -> {
+			if (empty != editor) {
+				OrderItem orderItem = editor.getValue();
+				items.remove(orderItem);
+				editors.remove(editor);
+				removeRegistration(priceChangeRegistration);
+				removeRegistration(productChangeRegistration);
+				getElement().removeChild(editor.getElement());
+				priceChanged();
+			}
+		});
+		return editor;
+	}
+
+	private void removeRegistration(Registration r) {
+		r.remove();
+		registrations.remove(r);
+	}
+
+	private Registration addRegistration(Registration r) {
+		registrations.add(r);
+		return r;
+	}
+	
 	@Override
 	public void setReadOnly(boolean readOnly) {
 		HasValue.super.setReadOnly(readOnly);
@@ -74,11 +109,11 @@ public class OrderItemsEdit extends PolymerTemplate<TemplateModel> implements Ha
 	public List<OrderItem> getValue() {
 		return items;
 	}
-	
-	void productChanged(OrderItemEdit item,Product product) {
-		if(empty == item) {
+
+	private void productChanged(OrderItemEdit item, Product product) {
+		if (empty == item) {
 			createEmptyElement();
-			OrderItem  orderItem = new OrderItem();
+			OrderItem orderItem = new OrderItem();
 			orderItem.setProduct(product);
 			items.add(orderItem);
 			item.setValue(orderItem);
@@ -86,42 +121,33 @@ public class OrderItemsEdit extends PolymerTemplate<TemplateModel> implements Ha
 		}
 	}
 
-	void priceChanged() {
+	private void priceChanged() {
 		Integer totalPrice = items.stream().filter(o -> o != null && o.getProduct() != null)
 				.mapToInt(o -> o.getProduct().getPrice() * o.getQuantity()).sum();
 		fireEvent(new PriceChangeEvent(totalPrice));
 	}
 
-	void deleteItem(OrderItemEdit item) {
-		if(empty != item) {
-			OrderItem  orderItem = item.getValue();
-			items.remove(orderItem);
-			editors.remove(item);
-			getElement().removeChild(item.getElement());
-			priceChanged();
-		}
-	}
-
 	private void createEmptyElement() {
-		empty = new OrderItemEdit(this,productSource);
-		editors.add(empty);
+		empty = createEditor(null);
 		getElement().appendChild(empty.getElement());
 	}
 
-	public Registration addPriceChangeListener(ComponentEventListener<PriceChangeEvent> listener)  {
+	public Registration addPriceChangeListener(ComponentEventListener<PriceChangeEvent> listener) {
 		return addListener(PriceChangeEvent.class, listener);
 	}
 
 	public class PriceChangeEvent extends ComponentEvent<OrderItemsEdit> {
 
 		private final Integer totalPrice;
+
 		PriceChangeEvent(Integer totalPrice) {
 			super(OrderItemsEdit.this, false);
 			this.totalPrice = totalPrice;
 		}
+
 		public Integer getTotalPrice() {
 			return totalPrice;
 		}
-		
+
 	}
 }
