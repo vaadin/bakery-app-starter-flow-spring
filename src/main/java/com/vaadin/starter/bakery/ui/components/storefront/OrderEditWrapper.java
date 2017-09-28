vaadin/bakery-app-starter-flow-spring

commit 9b4aada739fc5dd66d51027e1fe19de6c27d99f9

add a workaround for the order edit issue in Safari 11 (#165)
* add a workaround for the order edit issue in Safari 11

The issue happens because the <order-edit> and <order-detail> elements inserted into the light DOM of the <order-edit-wrapper> for some reason do not inherit any of custom CSS properties from their parent. That breaks the Valo theme for <vaadin-form-layout> which requires --valo-space-l to be defined. This might be a browser issue as it does not appear in Safari 10, nor in Chrome. Inserting the same elements at a later point in time (when opening the edit dialog) allows to avoid the issue.

Jira: BFF-270

* Merge branch 'master' into fix/270-order-edit

src/main/java/com/vaadin/starter/bakery/ui/components/storefront/OrderEditWrapper.java
@@ -32,9 +32,6 @@ public class OrderEditWrapper extends PolymerTemplate<OrderEditWrapper.Model> {
 	private Order order;
 
 	public OrderEditWrapper() {
-		addToSlot(this, orderEdit, "detail-dialog");
-		addToSlot(this, orderDetail, "detail-dialog");
-
 		orderEdit.addCancelListener(e -> fireEvent(new CancelEvent(e.hasChanges())));
 		orderEdit.addReviewListener(e -> this.review());
 		orderDetail.addBackListener(e -> this.edit());
@@ -42,6 +39,9 @@ public class OrderEditWrapper extends PolymerTemplate<OrderEditWrapper.Model> {
 	}
 
 	public void openEdit(Order order, User currentUser, Collection<Product> availableProducts) {
+		// no-op if orderEdit is already a child of `this`
+		addToSlot(this, orderEdit, "detail-dialog");
+
 		this.order = order;
 		orderEdit.init(currentUser, availableProducts);
 		orderEdit.setEditableItem(order);
@@ -62,6 +62,9 @@ public class OrderEditWrapper extends PolymerTemplate<OrderEditWrapper.Model> {
 	}
 
 	private void review() {
+		// no-op if orderDetail is already a child of `this`
+		addToSlot(this, orderDetail, "detail-dialog");
+
 		final boolean review = true;
 		orderDetail.display(order, review);
 		getModel().setReview(review);

commit d8fae7ad0a5576e301e4eb25fad826818e680ee9

fix the order edit view broken in Firefox after the commit 9b4aada (developer-preview)
It looks like a Firefox issue: if a custom element is removed from the DOM and then immediately inserted again, Firefox calls the element's disconnectedCallback() twice. In this case it was happening when the `<order-edit>` element was re-inserted as a child of the `<order-edit-wrapper>`.

Jira: BFF-297

(cherry picked from commit 158c78a)

src/main/java/com/vaadin/starter/bakery/ui/components/storefront/OrderEditWrapper.java
@@ -39,8 +39,12 @@ public class OrderEditWrapper extends PolymerTemplate<OrderEditWrapper.Model> {
 	}
 
 	public void openEdit(Order order, User currentUser, Collection<Product> availableProducts) {
-		// no-op if orderEdit is already a child of `this`
-		addToSlot(this, orderEdit, "detail-dialog");
+		// This is a workaround for a Safari 11 issue.
+		// If the orderEdit is injected into the page in the OrderEditWrapper constructor,
+		// Safari fails to set the styles correctly.
+		if (orderEdit.getElement().getParent() == null) {
+			addToSlot(this, orderEdit, "detail-dialog");
+		}
 
 		this.order = order;
 		orderEdit.init(currentUser, availableProducts);
@@ -62,8 +66,12 @@ public class OrderEditWrapper extends PolymerTemplate<OrderEditWrapper.Model> {
 	}
 
 	private void review() {
-		// no-op if orderDetail is already a child of `this`
-		addToSlot(this, orderDetail, "detail-dialog");
+		// This is a workaround for a Safari 11 issue.
+		// If the orderDetail is injected into the page in the OrderEditWrapper constructor,
+		// Safari fails to set the styles correctly.
+		if (orderDetail.getElement().getParent() == null) {
+			addToSlot(this, orderDetail, "detail-dialog");
+		}
 
 		final boolean review = true;
 		orderDetail.display(order, review);