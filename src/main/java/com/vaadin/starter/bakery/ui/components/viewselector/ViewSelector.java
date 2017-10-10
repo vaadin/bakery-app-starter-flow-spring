2b138220e29bdb643d62e401352fbd7584671b53
Tulio Garcia <tulio@vaadin.com>
2017-10-10 14:25:21 +0300

    Removed unused mixins in ViewSelector

--- src/main/java/com/vaadin/starter/bakery/ui/components/viewselector/ViewSelector.java
+++ src/main/java/com/vaadin/starter/bakery/ui/components/viewselector/ViewSelector.java
@@ -1,15 +1,10 @@
 package com.vaadin.starter.bakery.ui.components.viewselector;
 
-import static com.vaadin.starter.bakery.ui.utils.TemplateUtil.setSlot;
-
-import java.util.Arrays;
-
 import com.vaadin.annotations.HtmlImport;
 import com.vaadin.annotations.Tag;
 import com.vaadin.external.jsoup.helper.Validate;
 import com.vaadin.flow.template.PolymerTemplate;
 import com.vaadin.flow.template.model.TemplateModel;
-import com.vaadin.starter.bakery.ui.utils.TemplateUtil;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.HasComponents;
 
@@ -25,9 +20,4 @@ public class ViewSelector extends PolymerTemplate<TemplateModel> implements HasC
 		});
 	}
 
-	@Override
-	public void add(Component... components) {
-		HasComponents.super.add(components);
-		Arrays.stream(components).forEach(component -> setSlot(component, "detail-dialog"));
-	}
 }
