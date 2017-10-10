8ad3b0028454b3929d31eda7820eb6d1e2c6536b
Artur Signell <artur@vaadin.com>
2017-10-10 14:06:05 +0300

    Update Users view to use more element classes

---
+++ src/test/java/com/vaadin/starter/bakery/elements/PaperToastElement.java
@@ -0,0 +1,13 @@
+package com.vaadin.starter.bakery.elements;
+
+import com.vaadin.testbench.TestBenchElement;
+import com.vaadin.testbench.elementsbase.Element;
+
+@Element("paper-toast")
+public class PaperToastElement extends TestBenchElement {
+
+	@Override
+	public String getText() {
+		return getPropertyString("text");
+	}
+}
