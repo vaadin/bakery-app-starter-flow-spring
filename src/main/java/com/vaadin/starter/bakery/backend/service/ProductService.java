e426ac595d299c6d65bd591b895ae5ebee6ef9d2
Tulio Garcia <tulio@vaadin.com>
2017-10-12 11:46:29 +0300

    ProductService now uses constructor injection instead of bean locator

--- src/main/java/com/vaadin/starter/bakery/backend/service/ProductService.java
+++ src/main/java/com/vaadin/starter/bakery/backend/service/ProductService.java
@@ -2,21 +2,29 @@ package com.vaadin.starter.bakery.backend.service;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
-import com.vaadin.starter.bakery.app.BeanLocator;
 import com.vaadin.starter.bakery.backend.data.entity.Product;
 import com.vaadin.starter.bakery.repositories.ProductRepository;
 
 @Service
 public class ProductService implements CrudService<Product> {
 
+	private final ProductRepository productRepository;
+
+	@Autowired
+	public ProductService(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
+
 	public Page<Product> findAnyMatching(Optional<String> filter, Pageable pageable) {
 		if (filter.isPresent()) {
 			String repositoryFilter = "%" + filter.get() + "%";
-			return getRepository().findByNameLikeIgnoreCase(repositoryFilter, pageable);
+			return productRepository.findByNameLikeIgnoreCase(repositoryFilter, pageable);
 		} else {
 			return find(pageable);
 		}
@@ -25,18 +33,19 @@ public class ProductService implements CrudService<Product> {
 	public long countAnyMatching(Optional<String> filter) {
 		if (filter.isPresent()) {
 			String repositoryFilter = "%" + filter.get() + "%";
-			return getRepository().countByNameLikeIgnoreCase(repositoryFilter);
+			return productRepository.countByNameLikeIgnoreCase(repositoryFilter);
 		} else {
 			return count();
 		}
 	}
 
-	@Override
-	public ProductRepository getRepository() {
-		return BeanLocator.find(ProductRepository.class);
-	}
 
 	public Page<Product> find(Pageable pageable) {
-		return getRepository().findBy(pageable);
+		return productRepository.findBy(pageable);
+	}
+
+	@Override
+	public JpaRepository<Product, Long> getRepository() {
+		return productRepository;
 	}
 }
