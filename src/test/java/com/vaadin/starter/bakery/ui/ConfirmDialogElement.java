b2ed0ca30bf233c101476fadf4f95794c6652456
Artur <artur@vaadin.com>
2017-10-12 17:13:15 +0300

    simplify login test (use TestBench 6.0-SNAPSHOT)
    The LoginIT itself does not change much, but the LoginViewElement helper class becomes simpler (because TB6 allows selecting elements inside Shadow DOM using their tag names).

--- src/test/java/com/vaadin/starter/bakery/ui/ConfirmDialogElement.java
+++ src/test/java/com/vaadin/starter/bakery/ui/ConfirmDialogElement.java
@@ -1,9 +1,9 @@
 package com.vaadin.starter.bakery.ui;
 
 import com.vaadin.starter.bakery.By;
-import com.vaadin.testbench.elementsbase.AbstractElement;
+import com.vaadin.testbench.TestBenchElement;
 
-class ConfirmDialogElement extends AbstractElement {
+class ConfirmDialogElement extends TestBenchElement {
 	void confirm() {
 		findElement(By.shadowSelector("::shadow vaadin-button[theme~='danger']")).click();
 	}
--- src/test/java/com/vaadin/starter/bakery/ui/view/LoginViewElement.java
+++ src/test/java/com/vaadin/starter/bakery/ui/view/LoginViewElement.java
@@ -1,59 +1,32 @@
 package com.vaadin.starter.bakery.ui.view;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.vaadin.starter.bakery.By;
-import com.vaadin.testbench.elementsbase.AbstractElement;
+import com.vaadin.starter.bakery.elements.ButtonElement;
+import com.vaadin.starter.bakery.elements.PasswordFieldElement;
+import com.vaadin.starter.bakery.elements.TextFieldElement;
+import com.vaadin.testbench.TestBenchElement;
 
-
-public class LoginViewElement extends AbstractElement {
+public class LoginViewElement extends TestBenchElement {
 
 	public void login(String username, String password) {
-		WebElement loginElement = getLoginInput();
-		WebElement passwordElement = getPasswordInput();
-		loginElement.clear();
-		loginElement.sendKeys(username);
-		passwordElement.clear();
-		passwordElement.sendKeys(password);
+		getLogin().setValue(username);
+		getPassword().setValue(password);
+		getSignIn().click();
 
-		getSubmit().click();
-
-		waitUntilElementPresent(By.tagName("bakery-storefront"));
+		waitUntil(ExpectedConditions.presenceOfElementLocated(By.tagName("bakery-storefront")));
 	}
 
-	protected void waitUntilElementPresent(org.openqa.selenium.By by) {
-		waitUntil(ExpectedConditions.presenceOfElementLocated(by), 30);
+	public ButtonElement getSignIn() {
+		return $(ButtonElement.class).id("button-submit");
 	}
 
-	protected <T> void waitUntil(ExpectedCondition<T> condition, long timeoutInSeconds) {
-		new WebDriverWait(getDriver(), timeoutInSeconds).until(condition);
+	public PasswordFieldElement getPassword() {
+		return $(PasswordFieldElement.class).id("password");
 	}
 
-	private WebElement getSubmit() {
-		return findElement(By.shadowSelector("::shadow #button-submit"));
+	public TextFieldElement getLogin() {
+		return $(TextFieldElement.class).id("username");
 	}
-
-	private WebElement getPassword() {
-		return findElement(By.shadowSelector("::shadow #password"));
-	}
-
-	private WebElement getPasswordInput() {
-		return getPassword().findElement(By.shadowSelector("::shadow [part=value]"));
-	}
-
-	private WebElement getLogin() {
-		return findElement(By.shadowSelector("::shadow #username"));
-	}
-
-	private WebElement getLoginInput() {
-		return getLogin().findElement(By.shadowSelector("::shadow [part=value]"));
-	}
-
-	String getLoginLabel() {
-		return getLogin().getAttribute("label");
-	}
-
 }
