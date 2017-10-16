e8b10d23204177b69ebbaa059d162ea6a35eff6e
Artur <artur@vaadin.com>
2017-10-16 10:05:18 +0300

    Use waitForFirst instead of custom wait commands (#192)
    * Use waitForFirst instead of custom wait commands

---
+++ src/test/java/com/vaadin/starter/bakery/ui/view/StoreFrontViewElement.java
@@ -0,0 +1,9 @@
+package com.vaadin.starter.bakery.ui.view;
+
+import com.vaadin.testbench.TestBenchElement;
+import com.vaadin.testbench.elementsbase.Element;
+
+@Element("bakery-storefront")
+public class StoreFrontViewElement extends TestBenchElement {
+
+}
