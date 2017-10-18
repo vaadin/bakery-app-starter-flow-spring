9f3bab81c547ecb4aa606a2b799f11887210bdf1
Viktor Lukashov <viktor@vaadin.com>
2017-10-18 11:35:38 +0300

    remove a polymer template from the 404 page
    Jira: BFF-329

--- src/main/java/com/vaadin/starter/bakery/ui/NotFoundView.java
+++ src/main/java/com/vaadin/starter/bakery/ui/NotFoundView.java
@@ -1,24 +1,35 @@
 package com.vaadin.starter.bakery.ui;
 
-import com.vaadin.flow.model.TemplateModel;
+import com.vaadin.flow.dom.ElementFactory;
 import com.vaadin.flow.router.LocationChangeEvent;
 import com.vaadin.flow.router.View;
 import com.vaadin.hummingbird.ext.spring.annotations.ParentView;
 import com.vaadin.hummingbird.ext.spring.annotations.Route;
+import com.vaadin.router.RouterLink;
 import com.vaadin.router.Title;
 import com.vaadin.starter.bakery.ui.utils.BakeryConst;
-import com.vaadin.ui.Tag;
-import com.vaadin.ui.common.HtmlImport;
-import com.vaadin.ui.polymertemplate.PolymerTemplate;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Composite;
+import com.vaadin.ui.Text;
+import com.vaadin.ui.html.Div;
 
 import javax.servlet.http.HttpServletResponse;
 
-@Tag("bakery-404")
-@HtmlImport("context://src/404/bakery-404.html")
 @Route(value = BakeryConst.PAGE_NOTFOUND)
 @Title(BakeryConst.TITLE_NOT_FOUND)
 @ParentView(BakeryApp.class)
-public class NotFoundView extends PolymerTemplate<TemplateModel> implements View {
+public class NotFoundView extends Composite<Div> implements View {
+
+	public NotFoundView() {
+		// This should be simply `new RouterLink("Go to the front page.", StorefrontView.class)`
+		// but due to the Flow issue https://github.com/vaadin/flow/issues/1476, there needs to be a workaround.
+		// The workaround can be removed after upgrading to the official flow-spring plugin (see BFF-335)
+		RouterLink link = Component.from(
+				ElementFactory.createRouterLink("", "Go to the front page."),
+				RouterLink.class);
+		getContent().add(new Text("Oops you hit a 404. "), link);
+	}
+
 	@Override
 	public void onLocationChange(LocationChangeEvent locationChangeEvent) {
 		locationChangeEvent.setStatusCode(HttpServletResponse.SC_NOT_FOUND);
