8fef5811bd4ed3e2ceee45abb6504a940b1c23db
Tulio Garcia <tulio@vaadin.com>
2017-10-24 16:34:48 +0300

    Removed unused imports

--- src/main/java/com/vaadin/starter/bakery/ui/presenter/Confirmer.java
+++ src/main/java/com/vaadin/starter/bakery/ui/presenter/Confirmer.java
@@ -3,10 +3,7 @@
  */
 package com.vaadin.starter.bakery.ui.presenter;
 
-import com.vaadin.shared.Registration;
-import com.vaadin.starter.bakery.ui.event.DecisionEvent;
 import com.vaadin.starter.bakery.ui.messages.Message;
-import com.vaadin.ui.event.ComponentEventListener;
 
 public interface Confirmer {
 
