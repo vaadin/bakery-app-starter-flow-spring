f18ef84cb948565b6a7697cd106c230f218629f2
Alex <alex@vaadin.com>
2017-10-24 11:28:53 +0300

    Removed unused DTO classes

--- src/main/java/com/vaadin/starter/bakery/ui/dataproviders/OrdersDataProvider.java
+++ src/main/java/com/vaadin/starter/bakery/ui/dataproviders/OrdersDataProvider.java
@@ -7,17 +7,13 @@ import java.util.List;
 import java.util.Optional;
 
 import com.vaadin.starter.bakery.backend.data.entity.Order;
-import com.vaadin.starter.bakery.ui.entities.OrderTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.vaadin.starter.bakery.backend.data.DashboardData;
-import com.vaadin.starter.bakery.backend.data.OrderState;
-import com.vaadin.starter.bakery.backend.data.entity.OrderItem;
 import com.vaadin.starter.bakery.backend.service.OrderService;
-import com.vaadin.starter.bakery.ui.entities.Customer;
 import com.vaadin.starter.bakery.ui.utils.DashboardUtils.PageInfo;
 
 @Service
@@ -63,72 +59,4 @@ public class OrdersDataProvider {
 		return orderService;
 	}
 
-	public void addOrderComment(String orderId, String message) {
-		orderService.addComment(DataProviderUtil.readId(orderId), message);
-	}
-
-	public OrderTO getOrder(String orderId) {
-		Order dataEntity = findOrderById(orderId);
-		return toUIEntity(dataEntity);
-	}
-
-	private OrderTO toUIEntity(Order dataEntity) {
-		OrderTO order = new OrderTO();
-		fillOrder(order, dataEntity);
-		return order;
-	}
-
-	public void fillOrder(OrderTO uiEntity, Order dataEntity) {
-		new UIOrderFiller().fill(uiEntity, dataEntity);
-	}
-
-	private com.vaadin.starter.bakery.backend.data.entity.Order findOrderById(String orderId) {
-		Order dataEntity = getOrderService()
-				.findOrder(Long.valueOf(orderId));
-
-		return dataEntity;
-	}
-
-	/**
-	 *
-	 * Responsible for filling the UI OrderTO object with the data from model object.
-	 *
-	 */
-	static class UIOrderFiller {
-
-		void fill(OrderTO uiEntity, Order dataEntity) {
-			uiEntity.setId(dataEntity.getId().toString());
-			uiEntity.setDate(dataEntity.getDueDate().toString());
-			uiEntity.setTime(dataEntity.getDueTime().toString());
-
-			uiEntity.setPlace(dataEntity.getPickupLocation().getName());
-			uiEntity.setStatus(dataEntity.getState().getDisplayName());
-
-			fillUICustomerEntity(uiEntity.getCustomer(), dataEntity.getCustomer());
-			fillUIHistoryItems(uiEntity, dataEntity.getHistory());
-			fillUIGoods(uiEntity, dataEntity.getItems());
-		}
-
-		private void fillUICustomerEntity(Customer uiCustomer,
-				com.vaadin.starter.bakery.backend.data.entity.Customer dataEntity) {
-			uiCustomer.setDetails(dataEntity.getDetails());
-			uiCustomer.setName(dataEntity.getFullName());
-			uiCustomer.setNumber(dataEntity.getPhoneNumber());
-		}
-
-		private void fillUIHistoryItems(OrderTO uiEntity,
-				List<com.vaadin.starter.bakery.backend.data.entity.HistoryItem> historyItems) {
-			historyItems.forEach(h -> {
-				uiEntity.addHistoryItem(h.getTimestamp().toString(), h.getMessage(), h.getCreatedBy().getFirstName(),
-						DataProviderUtil.convertIfNotNull(h.getNewState(), OrderState::getDisplayName, () -> ""));
-			});
-		}
-
-		private void fillUIGoods(OrderTO uiEntity, List<OrderItem> orderItems) {
-			orderItems.forEach(item -> {
-				uiEntity.addUIGood(item.getQuantity(), item.getProduct().getName(), item.getProduct().getPrice(),
-						item.getComment());
-			});
-		}
-	}
 }
