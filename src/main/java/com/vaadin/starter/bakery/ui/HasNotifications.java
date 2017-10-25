68704f81b8788e863f01d170408618e7608babaf
Viktor Lukashov <viktor@vaadin.com>
2017-10-25 07:52:09 +0300

    rename `HasToast` into `HasNotifications`

---
+++ src/main/java/com/vaadin/starter/bakery/ui/HasNotifications.java
@@ -0,0 +1,19 @@
+package com.vaadin.starter.bakery.ui;
+
+import com.vaadin.ui.common.HasElement;
+
+/**
+ * 
+ * Unidirectional (server -> client) companion for client-side HasNotificationsMixin.
+ *
+ */
+public interface HasNotifications extends HasElement {
+
+	default void showNotification(String message) {
+		showNotification(message, false);
+	}
+
+	default void showNotification(String message, boolean persistent) {
+		getElement().callFunction("showNotification", message, persistent);
+	}
+}
