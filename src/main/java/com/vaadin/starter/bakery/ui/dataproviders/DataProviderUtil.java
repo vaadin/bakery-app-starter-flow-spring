8cd9814685cfc489cf5d499103a82e22a06542ff
Johannes Häyry <johannes.hayry@vaadin.com>
2017-11-02 11:13:00 +0200

    Remove obsolete methods and dependency from pom

--- src/main/java/com/vaadin/starter/bakery/ui/dataproviders/DataProviderUtil.java
+++ src/main/java/com/vaadin/starter/bakery/ui/dataproviders/DataProviderUtil.java
@@ -3,20 +3,7 @@ package com.vaadin.starter.bakery.ui.dataproviders;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-import com.google.gson.Gson;
-
-import elemental.json.JsonObject;
-
 public class DataProviderUtil {
-
-	public static <T> T toUIEntity(JsonObject json, Class<T> uiEntityClass) {
-		Gson gson = new Gson();
-		return gson.fromJson(json.toJson(), uiEntityClass);
-	}
-
-	public static Long readId(String id) {
-		return convertIfNotNull(id, Long::valueOf);
-	}
 
 	public static <S, T> T convertIfNotNull(S source, Function<S, T> converter) {
 		return convertIfNotNull(source, converter, () -> null);
