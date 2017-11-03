b98f71c5ebdcc047d6812494e55acb4b5541930d
Tulio Garcia <tulio@vaadin.com>
2017-11-03 10:34:44 +0200

    Fixed tests

--- src/test/java/com/vaadin/starter/bakery/ui/view/BakeryNavigationElement.java
+++ src/test/java/com/vaadin/starter/bakery/ui/view/BakeryNavigationElement.java
@@ -2,8 +2,6 @@ package com.vaadin.starter.bakery.ui.view;
 
 import com.vaadin.starter.bakery.elements.PaperTabElement;
 import com.vaadin.starter.bakery.ui.UsersViewElement;
-import com.vaadin.starter.bakery.ui.components.UserAvatarElement;
-import com.vaadin.starter.bakery.ui.components.UserPopupMenuElement;
 import com.vaadin.testbench.TestBenchElement;
 import com.vaadin.testbench.elementsbase.Element;
 
@@ -11,26 +9,23 @@ import com.vaadin.testbench.elementsbase.Element;
 public class BakeryNavigationElement extends TestBenchElement {
 
 	public DashboardViewElement navigateToDashboard() {
-		$(PaperTabElement.class).attribute("page-id", "dashboard").first().click();
-		return $(DashboardViewElement.class).onPage().waitForFirst();
+		return navigateTo("dashboard", DashboardViewElement.class);
 	}
 
 	public UsersViewElement navigateToUsers() {
-		$(PaperTabElement.class).attribute("page-id", "users").first().click();
-		return $(UsersViewElement.class).onPage().waitForFirst();
+		return navigateTo("users", UsersViewElement.class);
 	}
 
 	public ProductsViewElement navigateToProducts() {
-		$(PaperTabElement.class).attribute("page-id", "products").first().click();
-		return $(ProductsViewElement.class).onPage().waitForFirst();
+		return navigateTo("products", ProductsViewElement.class);
 	}
 
-	public void openUserPopupMenu() {
-		$(UserAvatarElement.class).first().click();
+	public LoginViewElement logout() {
+		return navigateTo("logout", LoginViewElement.class);
 	}
 
-	public UserPopupMenuElement getUserPopupMenu() {
-		return $(UserPopupMenuElement.class).first();
+	private <T extends TestBenchElement> T navigateTo(String pageId, Class<T> landingPage) {
+		$(PaperTabElement.class).attribute("page-id", pageId).first().click();
+		return $(landingPage).onPage().waitForFirst();
 	}
-
 }
