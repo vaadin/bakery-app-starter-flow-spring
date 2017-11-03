e82f992d1faabd708f0ca0b29f7e6023cf90f271
Johannes HÃ¤yry <johannes.hayry@vaadin.com>
2017-11-03 10:40:32 +0200

    Simplify SecurityConfig and move security specific stuff to right place

--- src/main/java/com/vaadin/starter/bakery/app/ApplicationConfiguration.java
+++ src/main/java/com/vaadin/starter/bakery/app/ApplicationConfiguration.java
@@ -1,37 +1,17 @@
 package com.vaadin.starter.bakery.app;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import java.util.Locale;
 
 import javax.annotation.PostConstruct;
-import java.util.Locale;
+
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Spring boot application configuration class.
- * <p>
- * For now it does:
- * <ul>
- * <li>Disables HTTP security (gives access to anyone to any page)</li>
- * </ul>
  *
  */
 @Configuration
 public class ApplicationConfiguration {
-
-	public static final String LOGIN_URL = "/login";
-	public static final String LOGOUT_URL = "/login?logout";
-	public static final String LOGIN_FAILURE_URL = "/login?error";
-	public static final String LOGIN_PROCESSING_URL = "/login";
-
-	/**
-	 * The password encoder to use when encrypting passwords.
-	 */
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
 
 	@PostConstruct
 	private void init() {
