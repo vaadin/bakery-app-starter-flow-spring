8e16cfdef6c07a56396174018413d2263712e2e5
Johannes Häyry <johannes.hayry@vaadin.com>
2017-11-07 11:30:21 +0200

    Simplify logout by letting Spring take care of it and just add link to navigation (#247)

---
+++ src/test/java/com/vaadin/starter/bakery/elements/AnchorElement.java
@@ -0,0 +1,9 @@
+package com.vaadin.starter.bakery.elements;
+
+import com.vaadin.testbench.TestBenchElement;
+import com.vaadin.testbench.elementsbase.Element;
+
+@Element("a")
+public class AnchorElement extends TestBenchElement {
+
+}
