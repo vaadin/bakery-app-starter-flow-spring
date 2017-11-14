1b4c753fe7a12c89fe4a2302b31174ece618782f
Tulio Garcia <tulio@vaadin.com>
2017-11-14 15:50:57 +0200

    Changed time combobox.
    Jira: BFF-374

--- src/main/java/com/vaadin/starter/bakery/ui/view/storefront/OrderEdit.java
+++ src/main/java/com/vaadin/starter/bakery/ui/view/storefront/OrderEdit.java
@@ -6,11 +6,18 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 
+import com.google.gwt.aria.client.State;
 import com.vaadin.data.BeanValidationBinder;
 import com.vaadin.data.ValidationException;
 import com.vaadin.data.provider.DataProvider;
@@ -32,6 +39,7 @@ import com.vaadin.ui.Tag;
 import com.vaadin.ui.button.Button;
 import com.vaadin.ui.combobox.ComboBox;
 import com.vaadin.ui.common.HtmlImport;
+import com.vaadin.ui.common.HasValue.ValueChangeListener;
 import com.vaadin.ui.datepicker.DatePicker;
 import com.vaadin.ui.event.ComponentEvent;
 import com.vaadin.ui.event.ComponentEventListener;
@@ -71,7 +79,7 @@ public class OrderEdit extends PolymerTemplate<OrderEdit.Model> {
 	private DatePicker date;
 
 	@Id("due-time")
-	private ComboBox<String> time;
+	private ComboBox<LocalTime> time;
 
 	@Id("pickup-location")
 	private ComboBox<String> pickupLocation;
@@ -114,19 +122,19 @@ public class OrderEdit extends PolymerTemplate<OrderEdit.Model> {
 		review.addClickListener(e -> fireEvent(new ReviewEvent()));
 
 		status.setDataProvider(DataProvider.ofItems(OrderState.values()));
-		status.addValueChangeListener(e -> {
-			getModel().setStatus(DataProviderUtil.toString(e.getValue()));
-		});
-		binder.forField(status).bind("state");
-
+		status.addValueChangeListener(createValueChangeListener(getModel()::setStatus, DataProviderUtil::toString));
 		date.setValue(LocalDate.now());
-		binder.forField(date).bind("dueDate");
 
-		ListDataProvider<String> timeDataProvider = DataProvider.fromStream(
-				IntStream.rangeClosed(8, 16).mapToObj(i -> localTimeConverter.toPresentation(LocalTime.of(i, 0))));
-		time.setDataProvider(timeDataProvider);
+		SortedSet<LocalTime> timeValues = IntStream.rangeClosed(8, 16).mapToObj(i -> LocalTime.of(i, 0))
+				.collect(Collectors.toCollection(TreeSet::new));
+		time.setItems(timeValues);
+		time.setItemLabelGenerator(localTimeConverter::toPresentation);
+		time.addCustomValueSetListener(e -> {
+			timeValues.add(localTimeConverter.toModel(e.getDetail()));
+			time.setItems(timeValues);
+		});
+		time.addValueChangeListener(createValueChangeListener(getModel()::setTime, localTimeConverter::toPresentation));
 
-		binder.forField(time).withConverter(localTimeConverter).bind("dueTime");
 		pickupLocation.setDataProvider(locationProvider);
 		pickupLocation.addValueChangeListener(e -> setHasChanges(true));
 		pickupLocation.setRequired(true);
@@ -144,6 +152,17 @@ public class OrderEdit extends PolymerTemplate<OrderEdit.Model> {
 		items.addListener(OrderItemsEdit.ValueChangeEvent.class, e -> review.setDisabled(!hasChanges()));
 		items.addListener(OrderItemsEdit.NewEditorEvent.class, e -> updateDesktopViewOnItemsEdit());
 		binder.addValueChangeListener(e -> review.setDisabled(!hasChanges()));
+	}
+
+	private <T> ValueChangeListener<ComboBox<T>, T> createValueChangeListener(Consumer<String> modelSetter,
+			Function<T, String> toStringConverter) {
+		return e -> {
+			String stringValue = toStringConverter.apply(e.getValue());
+			modelSetter.accept(stringValue != null ? stringValue : "");
+			if (e.getOldValue() != null) {
+				setHasChanges(true);
+			}
+		};
 	}
 
 	private void setHasChanges(boolean hasChanges) {
@@ -170,9 +189,11 @@ public class OrderEdit extends PolymerTemplate<OrderEdit.Model> {
 		getModel().setStatusValue("");
 		getModel().setPickupLocation("");
 		setTotalPrice(0);
+		hasChanges = false;
 	}
 
 	public void write(Order order) throws ValidationException {
+		order.setDueTime(time.getValue());
 		Query<String, String> locationQuery = new Query<>(0, 1, Collections.emptyList(), null,
 				pickupLocation.getValue());
 		locationProvider.findLocations(locationQuery).stream().findFirst().ifPresent(p -> order.setPickupLocation(p));
@@ -184,6 +205,7 @@ public class OrderEdit extends PolymerTemplate<OrderEdit.Model> {
 	public void read(Order order) {
 		binder.readBean(order);
 		title.setText(String.format("%s Order", order.isNew() ? "New" : "Edit"));
+		getModel().setTime(localTimeConverter.toPresentation(order.getDueTime()));
 
 		if (order.getState() != null) {
 			String status = orderStateConverter.toPresentation(order.getState());
@@ -194,6 +216,7 @@ public class OrderEdit extends PolymerTemplate<OrderEdit.Model> {
 		if (order.getPickupLocation() != null) {
 			getModel().setPickupLocation(order.getPickupLocation().getName());
 		}
+		hasChanges = false;
 		review.setDisabled(true);
 		updateDesktopViewOnItemsEdit();
 	}
