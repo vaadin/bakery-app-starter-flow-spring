16fbf00f9c05ecadd12f92f550b80a4a599aefe7
Viktor Lukashov <viktor@vaadin.com>
2017-11-15 16:43:40 +0200

    remove the workaround for flow/#2152
    Removing the workaround simplifies the application code:
    - remove reference to the BakeryNavigation component from the BakeryApp class (it was there to work around the issue https://github.com/vaadin/flow/issues/2152)
    - remove the 'page' model property of the BakeryApp component that's passed into BakeryNavigation via the polymer data binding
    - make the BakeryNavigation component itself observe 'after navigation' events to update the current page
    
    (waiting for the https://github.com/vaadin/flow/issues/2855 issue to be fixed)
    
    Jira: BFF-298

--- src/main/java/com/vaadin/starter/bakery/ui/BakeryApp.java
+++ src/main/java/com/vaadin/starter/bakery/ui/BakeryApp.java
@@ -8,30 +8,16 @@ import com.vaadin.starter.bakery.app.security.SecurityUtils;
 import com.vaadin.starter.bakery.ui.exceptions.AccessDeniedException;
 import com.vaadin.ui.Tag;
 import com.vaadin.ui.common.HtmlImport;
-import com.vaadin.ui.polymertemplate.Id;
 import com.vaadin.ui.polymertemplate.PolymerTemplate;
 
 @Tag("bakery-app")
 @HtmlImport("src/app/bakery-app.html")
-public class BakeryApp extends PolymerTemplate<BakeryApp.Model> implements RouterLayout, BeforeNavigationObserver {
-
-	@Id("navigation")
-	private BakeryNavigation navigation;
-
-	public interface Model extends TemplateModel {
-		void setPage(String page);
-	}
+public class BakeryApp extends PolymerTemplate<TemplateModel> implements RouterLayout, BeforeNavigationObserver {
 
 	@Override
 	public void beforeNavigation(BeforeNavigationEvent event) {
 		if (!SecurityUtils.isAccessGranted(event.getNavigationTarget())) {
 			event.rerouteToError(AccessDeniedException.class);
-			return;
 		}
-		String path = event.getLocation().getFirstSegment();
-		if (!path.isEmpty()) {
-			getModel().setPage(path);
-		}
-		navigation.updateNavigationBar();
 	}
 }
