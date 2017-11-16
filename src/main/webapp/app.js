0dac3c3e4afee940c1a6d71d002ef5420c861da6
Jens Jansson <peppe@vaadin.com>
2017-11-16 14:46:24 +0200

    ALL THE RULES!

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js').then(function (registration) {
-  }).catch(function (err) {
+  navigator.serviceWorker.register('sw.js').then(function(registration) {
+  }).catch(function(err) {
     // registration failed :(
     console.error('ServiceWorker registration failed: ', err);
   });
--- src/main/webapp/sw.js
+++ src/main/webapp/sw.js
@@ -3,44 +3,44 @@ var version = 1;
 
 function updateCache() {
   return caches.open(staticCacheName + version)
-    .then(function (cache) {
+    .then(function(cache) {
       return cache.addAll([
         'offline-page.html',
         'manifest.json',
         'images/login-banner.jpg',
         'icons/icon-96.png',
         'icons/icon-192.png',
-         'icons/icon-512.png'
-       ]);
+        'icons/icon-512.png'
+      ]);
     });
-};
+}
 
-self.addEventListener('install', function (event) {
+self.addEventListener('install', function (event){
   event.waitUntil(updateCache());
 });
 
-var doesRequestAcceptHtml = function (request) {
+var doesRequestAcceptHtml = function (request){
   return request.headers.get('Accept')
     .split(',')
-    .some(function (type) {
+    .some(function (type){
       return type === 'text/html';
     });
 };
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function (event){
   var request = event.request;
   if (doesRequestAcceptHtml(request)) {
     // HTML pages fallback to offline page
     event.respondWith(
       fetch(request)
-        .catch(function () {
+        .catch(function (){
           return caches.match('offline-page.html');
         })
     );
   } else {
     event.respondWith(
       caches.match(request)
-        .then(function (response) {
+        .then(function (response){
           return response || fetch(request);
         })
     );
