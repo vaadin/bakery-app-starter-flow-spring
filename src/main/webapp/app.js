31023b7e2cd35172ae6aa381405e6c95d7888f19
Jens Jansson <peppe@vaadin.com>
2017-11-16 14:39:41 +0200

    reformatted javascript files

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,9 +1,7 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-    });
+  navigator.serviceWorker.register('sw.js').then(function (registration) {
+  }).catch(function (err) {
+    // registration failed :(
+    console.error('ServiceWorker registration failed: ', err);
+  });
 }
--- src/main/webapp/sw.js
+++ src/main/webapp/sw.js
@@ -2,53 +2,47 @@ var staticCacheName = 'static';
 var version = 1;
 
 function updateCache() {
-    return caches.open(staticCacheName + version)
-        .then(function (cache) {
-            return cache.addAll([
-                'offline-page.html',
-                'manifest.json',
-                'images/login-banner.jpg',
-                'icons/icon-96.png',
-                'icons/icon-192.png',
-                'icons/icon-512.png'
-            ]);
-        });
+  return caches.open(staticCacheName + version)
+    .then(function (cache) {
+      return cache.addAll([
+        'offline-page.html',
+        'manifest.json',
+        'images/login-banner.jpg',
+        'icons/icon-96.png',
+        'icons/icon-192.png',
+         'icons/icon-512.png'
+       ]);
+    });
 };
 
 self.addEventListener('install', function (event) {
-    event.waitUntil(updateCache());
+  event.waitUntil(updateCache());
 });
 
 var doesRequestAcceptHtml = function (request) {
-    return request.headers.get('Accept')
-        .split(',')
-        .some(function (type) { return type === 'text/html'; });
+  return request.headers.get('Accept')
+    .split(',')
+    .some(function (type) {
+      return type === 'text/html';
+    });
 };
 
 self.addEventListener('fetch', function (event) {
-    var request = event.request;
-    if (doesRequestAcceptHtml(request)) {
-
-// HTML pages fallback to offline page
-
-        event.respondWith(
-            fetch(request)
-                .catch(function () {
-                    return caches.match('offline-page.html');
-                })
-        );
-    } else {
-
-// Default fetch behaviour
-
-
-// Cache first for all other requests
-
-        event.respondWith(
-            caches.match(request)
-                .then(function (response) {
-                    return response || fetch(request);
-                })
-        );
-    }
+  var request = event.request;
+  if (doesRequestAcceptHtml(request)) {
+    // HTML pages fallback to offline page
+    event.respondWith(
+      fetch(request)
+        .catch(function () {
+          return caches.match('offline-page.html');
+        })
+    );
+  } else {
+    event.respondWith(
+      caches.match(request)
+        .then(function (response) {
+          return response || fetch(request);
+        })
+    );
+  }
 });
