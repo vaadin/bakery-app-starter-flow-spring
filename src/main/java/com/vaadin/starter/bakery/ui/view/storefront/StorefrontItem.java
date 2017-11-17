7818120df65cc56a62ebf011bbaecc61d129bb95
Tulio Garcia <tulio@vaadin.com>
2017-11-17 09:29:58 +0200

    Moving date logic to StorefrontItem (WIP)
    BFF-395

--- src/main/java/com/vaadin/starter/bakery/ui/view/storefront/StorefrontItem.java
+++ src/main/java/com/vaadin/starter/bakery/ui/view/storefront/StorefrontItem.java
@@ -1,5 +1,10 @@
 package com.vaadin.starter.bakery.ui.view.storefront;
 
+import java.time.LocalDate;
+import java.time.temporal.TemporalField;
+import java.time.temporal.WeekFields;
+import java.util.Locale;
+
 import com.vaadin.flow.model.Convert;
 import com.vaadin.flow.model.Include;
 import com.vaadin.flow.model.TemplateModel;
@@ -18,7 +23,7 @@ public class StorefrontItem extends PolymerTemplate<StorefrontItem.Model> {
 
 	public interface Model extends TemplateModel {
 		@Include({ "id", "dueDate.day", "dueDate.weekday", "dueDate.date", "dueTime", "state", "pickupLocation.name",
-				"customer.fullName", "items.product.name", "items.quantity" })
+			"customer.fullName", "items.product.name", "items.quantity" })
 		@Convert(value = LongToStringConverter.class, path = "id")
 		@Convert(value = StorefrontLocalDateConverter.class, path = "dueDate")
 		@Convert(value = LocalTimeConverter.class, path = "dueTime")
@@ -32,5 +37,44 @@ public class StorefrontItem extends PolymerTemplate<StorefrontItem.Model> {
 
 	public void setOrder(Order order) {
 		getModel().setItem(order);
+	}
+
+	boolean isYesterday(LocalDate date) {
+		return LocalDate.now().minusDays(1).equals(date);
+	}
+
+	boolean isToday(LocalDate date) {
+		return LocalDate.now().equals(date);
+	}
+
+	boolean isThisWeekBeforeYesterday(LocalDate date) {
+		LocalDate now = LocalDate.now();
+		return isSameWeek(now, date) && date.isBefore(now.minusDays(1));
+	}
+
+	private boolean isSameWeek(LocalDate date1, LocalDate date2) {
+		TemporalField woy = WeekFields.of(Locale.getDefault()).weekOfWeekBasedYear();
+		return date1.get(woy) == date2.get(woy);
+	}
+
+	boolean isThisWeekStartingTomorrow(LocalDate date) {
+		LocalDate now = LocalDate.now();
+		return isSameWeek(now, date) && date.isAfter(now);
+	}
+
+	boolean isUpcoming(LocalDate date) {
+		return moment(new Date(date)).isAfter(moment().endOf('week').add(1, 'day'));
+	}
+
+	String getShortDay(LocalDate date) {
+		return moment(new Date(date)).format('ddd D');
+	}
+
+	String getFullDay(LocalDate date) {
+		return moment(new Date(date)).format('dddd');
+	}
+
+	String getMonth(LocalDate date) {
+		return moment(new Date(date)).format('MMM D');
 	}
 }
