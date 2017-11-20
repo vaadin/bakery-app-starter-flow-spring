64e0198aa588b3d434c571ed1501060794fc189c
Tulio Garcia <tulio@vaadin.com>
2017-11-20 14:48:47 +0200

    Removed unused import

--- src/main/java/com/vaadin/starter/bakery/ui/view/storefront/StorefrontItem.java
+++ src/main/java/com/vaadin/starter/bakery/ui/view/storefront/StorefrontItem.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
