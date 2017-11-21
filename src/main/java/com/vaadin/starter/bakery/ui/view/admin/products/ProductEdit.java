9f7304e313020f14e8c64b56c910ff26edc44469
tuliogarcia <tulio@vaadin.com>
2017-11-21 11:38:12 +0200

    Simplify or remove binder converter (#279)
    * Removed BinderConverter
    
    Jira: BFF-394
    
    * Static import
    
    * Fix pom to not use snapshots

--- src/main/java/com/vaadin/starter/bakery/ui/view/admin/products/ProductEdit.java
+++ src/main/java/com/vaadin/starter/bakery/ui/view/admin/products/ProductEdit.java
@@ -1,5 +1,6 @@
 package com.vaadin.starter.bakery.ui.view.admin.products;
 
+import static com.vaadin.starter.bakery.ui.dataproviders.DataProviderUtil.convertIfNotNull;
 import static com.vaadin.starter.bakery.ui.utils.FormattingUtils.DECIMAL_ZERO;
 
 import java.math.BigDecimal;
@@ -9,18 +10,18 @@ import java.util.Currency;
 import java.util.Locale;
 
 import com.vaadin.data.BeanValidationBinder;
+import com.vaadin.data.Converter;
 import com.vaadin.data.Result;
 import com.vaadin.data.ValidationException;
 import com.vaadin.data.ValueContext;
 import com.vaadin.flow.model.TemplateModel;
 import com.vaadin.starter.bakery.backend.data.entity.Product;
-import com.vaadin.starter.bakery.ui.utils.converters.binder.BinderConverter;
-import com.vaadin.starter.bakery.ui.view.admin.EditForm;
-import com.vaadin.starter.bakery.ui.view.admin.EntityEditor;
 import com.vaadin.starter.bakery.ui.event.CancelEvent;
 import com.vaadin.starter.bakery.ui.event.DeleteEvent;
 import com.vaadin.starter.bakery.ui.event.SaveEvent;
 import com.vaadin.starter.bakery.ui.utils.FormattingUtils;
+import com.vaadin.starter.bakery.ui.view.admin.EditForm;
+import com.vaadin.starter.bakery.ui.view.admin.EntityEditor;
 import com.vaadin.ui.Tag;
 import com.vaadin.ui.common.HtmlImport;
 import com.vaadin.ui.html.Span;
@@ -78,25 +79,24 @@ public class ProductEdit extends PolymerTemplate<TemplateModel> implements Entit
 		return binder.hasChanges();
 	}
 
-	class PriceConverter implements BinderConverter<String, Integer> {
+	class PriceConverter implements Converter<String, Integer> {
 
 		@Override
-		public Result<Integer> convertToModelIfNotNull(String presentationValue, ValueContext valueContext) {
+		public Result<Integer> convertToModel(String presentationValue, ValueContext valueContext) {
 			try {
-				return Result.ok((int) Math.round(df.parse(presentationValue).doubleValue() * 100));
+				if (presentationValue == null || presentationValue.isEmpty()) {
+					return Result.error("Price is required");
+				} else {
+					return Result.ok((int) Math.round(df.parse(presentationValue).doubleValue() * 100));
+				}
 			} catch (ParseException e) {
 				return Result.error("Invalid value");
 			}
 		}
 
 		@Override
-		public Result<Integer> convertNullToModel(String presentationValue, ValueContext valueContext) {
-			return Result.error("Price is required");
-		}
-
-		@Override
-		public String convertToPresentationIfNotNull(Integer modelValue, ValueContext valueContext) {
-			return df.format(BigDecimal.valueOf(modelValue, 2));
+		public String convertToPresentation(Integer modelValue, ValueContext valueContext) {
+			return convertIfNotNull(modelValue, i -> df.format(BigDecimal.valueOf(i, 2)));
 		}
 	}
 }
