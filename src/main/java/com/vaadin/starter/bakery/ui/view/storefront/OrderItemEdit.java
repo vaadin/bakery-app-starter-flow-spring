9c68b1a1350017892cb24b64520372412d7a8c96
Tulio Garcia <tulio@vaadin.com>
2017-11-22 09:13:02 +0200

    Extracted CommentChangeEvent
    Jira: BFF-408

--- src/main/java/com/vaadin/starter/bakery/ui/view/storefront/OrderItemEdit.java
+++ src/main/java/com/vaadin/starter/bakery/ui/view/storefront/OrderItemEdit.java
@@ -6,6 +6,7 @@ import com.vaadin.shared.Registration;
 import com.vaadin.starter.bakery.backend.data.entity.OrderItem;
 import com.vaadin.starter.bakery.backend.data.entity.Product;
 import com.vaadin.starter.bakery.ui.utils.FormattingUtils;
+import com.vaadin.starter.bakery.ui.view.storefront.event.CommentChangeEvent;
 import com.vaadin.starter.bakery.ui.view.storefront.event.PriceChangeEvent;
 import com.vaadin.starter.bakery.ui.view.storefront.event.ProductChangeEvent;
 import com.vaadin.starter.bakery.ui.view.wrapper.ComboboxBinderWrapper;
@@ -124,21 +125,6 @@ public class OrderItemEdit extends PolymerTemplate<TemplateModel> implements Has
 
 	public Registration addDeleteListener(ComponentEventListener<DeleteEvent> listener) {
 		return addListener(DeleteEvent.class, listener);
-	}
-
-	public static class CommentChangeEvent extends ComponentEvent<OrderItemEdit> {
-
-		private final String comment;
-
-		CommentChangeEvent(OrderItemEdit component, String comment) {
-			super(component, false);
-			this.comment = comment;
-		}
-
-		public String getComment() {
-			return comment;
-		}
-
 	}
 
 	public static class DeleteEvent extends ComponentEvent<OrderItemEdit> {
