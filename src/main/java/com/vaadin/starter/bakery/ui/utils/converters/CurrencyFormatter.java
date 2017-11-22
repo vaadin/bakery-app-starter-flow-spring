b6778b5b24c4f79d915d530935dcc0db433dc57f
Tulio Garcia <tulio@vaadin.com>
2017-11-22 14:38:24 +0200

    Removed duplicated code.
    Jira: BFF-406

--- src/main/java/com/vaadin/starter/bakery/ui/utils/converters/CurrencyFormatter.java
+++ src/main/java/com/vaadin/starter/bakery/ui/utils/converters/CurrencyFormatter.java
@@ -1,19 +1,14 @@
 package com.vaadin.starter.bakery.ui.utils.converters;
 
-import static com.vaadin.starter.bakery.ui.utils.BakeryConst.APP_LOCALE;
-
-import java.math.BigDecimal;
-import java.text.NumberFormat;
-
 import com.vaadin.flow.model.ModelConverter;
 import com.vaadin.starter.bakery.ui.dataproviders.DataProviderUtil;
+import com.vaadin.starter.bakery.ui.utils.FormattingUtils;
 
 public class CurrencyFormatter implements ModelConverter<Integer, String> {
 
 	@Override
 	public String toPresentation(Integer modelValue) {
-		return DataProviderUtil.convertIfNotNull(modelValue,
-				v -> NumberFormat.getCurrencyInstance(APP_LOCALE).format(BigDecimal.valueOf(v, 2)));
+		return DataProviderUtil.convertIfNotNull(modelValue, FormattingUtils::formatAsCurrency);
 	}
 
 	@Override
