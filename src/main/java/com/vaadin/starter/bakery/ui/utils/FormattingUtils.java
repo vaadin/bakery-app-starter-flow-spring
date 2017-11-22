02c6b285fd8b7c960b1b19406cf42ef83d17394b
Tulio Garcia <tulio@vaadin.com>
2017-11-22 14:00:04 +0200

    Using BakeryConst.APP_LOCALE for defining the locale in FormattingUtils.
    Jira: BFF-406

--- src/main/java/com/vaadin/starter/bakery/ui/utils/FormattingUtils.java
+++ src/main/java/com/vaadin/starter/bakery/ui/utils/FormattingUtils.java
@@ -4,14 +4,13 @@ import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
-import java.util.Locale;
 
 public class FormattingUtils {
 
 	public static final String DECIMAL_ZERO = "0.00";
 
 	public static String formatAsCurrency(int valueInCents) {
-		return NumberFormat.getCurrencyInstance(Locale.US).format(BigDecimal.valueOf(valueInCents, 2));
+		return NumberFormat.getCurrencyInstance(BakeryConst.APP_LOCALE).format(BigDecimal.valueOf(valueInCents, 2));
 	}
 
 	public static DecimalFormat getUiPriceFormatter() {
