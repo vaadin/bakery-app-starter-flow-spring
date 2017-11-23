b4da046ff9a92ba78e95da93d3dcf62c22105a17
alexberazouski <alex@vaadin.com>
2017-11-23 09:25:24 +0200

    Launch Application from IDE (#285)
    * Modified pom
    
    updated versions of Spring-boot and maven-war-plugin
    removed obsolete properties
    Moved dev frontend parameter to application.properties
    
    jira: BFF-404
    
    * Make possible to start app from IDE
    
    Moved Application class in app package

--- src/main/java/com/vaadin/starter/bakery/Application.java
+++ src/main/java/com/vaadin/starter/bakery/Application.java
@@ -3,6 +3,7 @@ package com.vaadin.starter.bakery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -31,5 +32,10 @@ public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
 	}
 }
