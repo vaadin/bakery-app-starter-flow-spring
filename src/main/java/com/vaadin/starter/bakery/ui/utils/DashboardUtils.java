4565969cd0fd3cb63fbfb4833365093e68b2131a
tuliogarcia <tulio@vaadin.com>
2017-11-24 15:29:35 +0200

    Improve code in dashboardutils (#292)
    * Removed unused import
    
    * Changed implementation of getDeliveriesThisMonthCategories
    
    Improve readability.
    
    * Removed getDeliveriesThisMonthCategories
    
    * Simplified more code.
    
    Moved month formatting to FormattingUtils
    
    * Added comments and test.
    
    * Added comment.
    
    * Extracted createSubtitle method.

--- src/main/java/com/vaadin/starter/bakery/ui/utils/DashboardUtils.java
+++ src/main/java/com/vaadin/starter/bakery/ui/utils/DashboardUtils.java
@@ -1,19 +1,18 @@
 package com.vaadin.starter.bakery.ui.utils;
 
+import static com.vaadin.starter.bakery.ui.utils.FormattingUtils.getFullMonthName;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.MonthDay;
 import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import com.vaadin.starter.bakery.backend.data.DeliveryStats;
 import com.vaadin.starter.bakery.backend.data.OrderState;
@@ -24,27 +23,6 @@ import com.vaadin.starter.bakery.ui.entities.chart.ProductDeliveriesChartData;
 
 public class DashboardUtils {
 
-	private static String getDeliveriesThisYearTitle() {
-		return "Deliveries in " + Calendar.getInstance().get(Calendar.YEAR);
-	}
-
-	private static List<String> getDeliveriesThisYearCategories() {
-		return Arrays.asList("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
-	}
-
-	private static List<String> getDeliveriesThisMonthCategories(List<?> list) {
-		AtomicInteger dayNumber = new AtomicInteger(0);
-		return list.stream().map(o -> "" + dayNumber.incrementAndGet()).collect(Collectors.toList());
-	}
-
-	private static String getCurrentMonthName() {
-		return Calendar.getInstance().getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.ENGLISH);
-	}
-
-	private static String getDeliveriesThisMonthTitle() {
-		return "Deliveries in " + getCurrentMonthName();
-	}
-
 	private static List<Integer> convertNumbersToIntegers(List<Number> numbersList) {
 		if (numbersList == null)
 			return Collections.emptyList();
@@ -53,22 +31,29 @@ public class DashboardUtils {
 				return (Integer) n.intValue();
 			return 0;
 		}).collect(Collectors.toList());
-
 	}
 
 	public static ColumnChartData getDeliveriesThisYearChartData(List<Number> deliveriesThisYear) {
-		return new ColumnChartData(getDeliveriesThisYearTitle(), "per Month",
-				convertNumbersToIntegers(deliveriesThisYear), getDeliveriesThisYearCategories());
+		List<String> deliveriesThisYearCategories = Arrays.asList("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
+				"Aug", "Sep", "Oct", "Nov", "Dec");
+		String deliveriesThisYearTitle = "Deliveries in " + LocalDate.now().getYear();
+		return new ColumnChartData(deliveriesThisYearTitle, "per Month",
+				convertNumbersToIntegers(deliveriesThisYear), deliveriesThisYearCategories);
 	}
 
 	public static ColumnChartData getDeliveriesThisMonthChartData(List<Number> deliveriesThisMonth) {
-		return new ColumnChartData(getDeliveriesThisMonthTitle(), "per Day",
-				convertNumbersToIntegers(deliveriesThisMonth), getDeliveriesThisMonthCategories(deliveriesThisMonth));
+		// A range going from 1 to the number of items in deliveriesThisMonth
+		List<String> deliveriesThisMonthCategories = IntStream.rangeClosed(1, deliveriesThisMonth.size())
+				.mapToObj(String::valueOf).collect(Collectors.toList());
+
+		String deliveriesThisMonthTitle = "Deliveries in " + getFullMonthName(LocalDate.now());
+		return new ColumnChartData(deliveriesThisMonthTitle, "per Day",
+				convertNumbersToIntegers(deliveriesThisMonth), deliveriesThisMonthCategories);
 	}
 
 	public static ProductDeliveriesChartData getDeliveriesPerProductPieChartData(
 			Map<Product, Integer> productDeliveries) {
-		return new ProductDeliveriesChartData("Products delivered in " + getCurrentMonthName(), "count",
+		return new ProductDeliveriesChartData("Products delivered in " + getFullMonthName(LocalDate.now()), "count",
 				productDeliveries);
 	}
 
@@ -141,33 +126,32 @@ public class DashboardUtils {
 		return ordersCountData;
 	}
 
+	public static OrdersCountData getNewOrdersCountData(DeliveryStats deliveryStats, Order lastOrder) {
+		return new OrdersCountData("New", createSubtitle(lastOrder), deliveryStats.getNewOrders());
+	}
+
 	private static final String NEW_ORDERS_COUNT_SUBTITLE_PATTERN = "Last %d%s ago";
 
-	public static OrdersCountData getNewOrdersCountData(DeliveryStats deliveryStats, Order lastOrder) {
-		OrdersCountData ordersCountData = new OrdersCountData("New", null, deliveryStats.getNewOrders());
+	private static String createSubtitle(Order lastOrder) {
 		LocalDateTime currTime = LocalDateTime.now();
-
 		LocalDateTime timestamp = lastOrder.getHistory().get(0).getTimestamp();
 
 		long value = timestamp.until(currTime, ChronoUnit.DAYS);
 		if (value > 0) {
-			ordersCountData.setSubtitle(String.format(NEW_ORDERS_COUNT_SUBTITLE_PATTERN, value, "d"));
-			return ordersCountData;
+			return String.format(NEW_ORDERS_COUNT_SUBTITLE_PATTERN, value, "d");
 		}
 
 		value = timestamp.until(currTime, ChronoUnit.HOURS);
 		if (value > 0) {
-			ordersCountData.setSubtitle(String.format(NEW_ORDERS_COUNT_SUBTITLE_PATTERN, value, "h"));
-			return ordersCountData;
+			return String.format(NEW_ORDERS_COUNT_SUBTITLE_PATTERN, value, "h");
 		}
+
 		value = timestamp.until(currTime, ChronoUnit.MINUTES);
 		if (value > 0) {
-			ordersCountData.setSubtitle(String.format(NEW_ORDERS_COUNT_SUBTITLE_PATTERN, value, "m"));
-			return ordersCountData;
+			return String.format(NEW_ORDERS_COUNT_SUBTITLE_PATTERN, value, "m");
 		}
 
 		// option if data contain orders from the future
-		ordersCountData.setSubtitle("Last just added");
-		return ordersCountData;
+		return "Last just added";
 	}
 }
