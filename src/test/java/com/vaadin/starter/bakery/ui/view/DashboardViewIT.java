64720fd6df04db918a7b7fe41fa6720338f6400d
alexberazouski <alex@vaadin.com>
2017-12-14 16:25:53 +0200

    Refactored Counter labels on dashboard (#328)
    * Use the Flow Charts API for the first row charts
    * Fix NPE when session without authentication
    
    Jira: BFF-452

--- src/test/java/com/vaadin/starter/bakery/ui/view/DashboardViewIT.java
+++ src/test/java/com/vaadin/starter/bakery/ui/view/DashboardViewIT.java
@@ -2,7 +2,6 @@ package com.vaadin.starter.bakery.ui.view;
 
 import com.vaadin.starter.bakery.AbstractIT;
 import com.vaadin.starter.bakery.ui.components.DashboardLCounterLabelElement;
-import com.vaadin.starter.bakery.ui.components.DashboardOrdersCounterElement;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -22,11 +21,8 @@ public class DashboardViewIT extends AbstractIT {
 	@Test
 	public void checkCounters() {
 		DashboardViewElement dashboardPage = openDashboardPage();
-		int numCounters = dashboardPage.getBoard().getRows().get(0).$(DashboardOrdersCounterElement.class).all().size();
 		int numLabels = dashboardPage.getBoard().getRows().get(0).$(DashboardLCounterLabelElement.class).all().size();
-		Assert.assertEquals(1, numCounters);
-		Assert.assertEquals(3, numLabels);
-		
+		Assert.assertEquals(4, numLabels);
 	}
 
 }
