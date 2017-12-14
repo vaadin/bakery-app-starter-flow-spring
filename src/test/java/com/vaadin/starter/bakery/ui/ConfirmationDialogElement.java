087cd057d5f3c875935a301fd8f5ccf208b08f36
Manolo Carrasco <manolo@apache.org>
2017-12-14 16:49:13 +0100

    Restore TB class for clicking on Confirmation

---
+++ src/test/java/com/vaadin/starter/bakery/ui/ConfirmationDialogElement.java
@@ -0,0 +1,17 @@
+package com.vaadin.starter.bakery.ui;
+
+import com.vaadin.starter.bakery.elements.ButtonElement;
+import com.vaadin.testbench.TestBenchElement;
+import com.vaadin.testbench.elementsbase.Element;
+
+@Element("starter-confirm-dialog")
+public class ConfirmationDialogElement extends TestBenchElement {
+
+	public void confirm() {
+		getOkButton().click();
+	}
+
+	public ButtonElement getOkButton() {
+		return $(ButtonElement.class).first();
+	}
+}
