11cbdbbca32a6f69278bccbb55d115098a88fe0b
Alex <alex@vaadin.com>
2017-12-15 10:29:56 +0200

    Deploy build to performance test server

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 PORT=5196
+PERF_PORT=5202
 scp -o StrictHostKeyChecking=no -P $PORT target/bakery-app-starter-flow-spring-*.war dev@app.fi:bakery.war
+scp -o StrictHostKeyChecking=no -P $PERF_PORT target/bakery-app-starter-flow-spring-*.war dev@app.fi:bakery.war
 ssh -o StrictHostKeyChecking=no -p $PORT dev@app.fi mv bakery.war tomcat/webapps/ROOT.war
+ssh -o StrictHostKeyChecking=no -p $PERF_PORT dev@app.fi mv bakery.war webapps/ROOT.war
 
