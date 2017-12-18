vaadin/bakery-app-starter-flow-spring

commit e8bf712eedc369b10167896113c02a410129a44e

Move login-specific initial page setup into the LoginView (using InitialPageSettings)
Jira: BFF-455


- remove unused global CSS properties (--app-primary-color)
 - update the valo-colors style module include (valo-color instead of valo-colors)
 - simplify the custom-style block necessary for the main document
 - remove the unused global CSS properties
 - reformat the spring Web security config

src/main/java/com/vaadin/starter/bakery/app/CustomBootstrapListener.java
@@ -7,16 +7,6 @@ import org.jsoup.nodes.Element;
 public class CustomBootstrapListener implements BootstrapListener {
 	public void modifyBootstrapPage(BootstrapPageResponse response) {
 		final Element head = response.getDocument().head();
-		if ("/login".equals(response.getRequest().getPathInfo())) {
-			// Force login page to use Shady DOM to avoid problems with browsers and
-			// password managers not supporting shadow DOM
-			head.prepend(
-					"<script type='text/javascript'>" +
-							"window.customElements=window.customElements||{};" +
-							"window.customElements.forcePolyfill=true;" +
-							"window.ShadyDOM={force:true};" +
-					"</script>");
-		}
 
 		// manifest needs to be prepended before scripts or it won't be loaded
 		head.prepend("<meta name=\"theme-color\" content=\"#227aef\">");
@@ -40,21 +30,7 @@ public class CustomBootstrapListener implements BootstrapListener {
 				"<meta name=\"viewport\" content=\"width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes\">");
 		head.append("<!-- Add any global styles for body, document, etc. -->\n" +
                 "    <custom-style>\n" +
-                "      <style is=\"custom-style\" include=\"valo-colors valo-typography\">\n" +
-                "        html,\n" +
-                "        body {\n" +
-                "          height: 100%;\n" +
-                "        }\n" +
-                "\n" +
-                "        body {\n" +
-                "          margin: 0;\n" +
-                "          background: var(--valo-shade-10pct);\n" +
-                "        }\n" +
-                "\n" +
-                "        bakery-app {\n" +
-                "          height: 100%;\n" +
-                "        }\n" +
-                "      </style>\n" +
+                "      <style is=\"custom-style\" include=\"valo-color valo-typography\"></style>\n" +
                 "    </custom-style>");
 	}
 }

src/main/webapp/frontend/src/app/bakery-app.html
@@ -8,12 +8,10 @@
   <template>
     <style>
       :host {
-        --app-primary-color: var(--valo-primary-color);
-        --app-secondary-color: var(--valo-body-text-color);
-
         height: 100%;
         display: flex;
         flex-direction: column-reverse;
+        background: var(--valo-shade-10pct);
         color: var(--valo-body-text-color);
         font-family: var(--valo-font-family);
       }

src/main/java/com/vaadin/starter/bakery/app/security/SecurityConfiguration.java
@@ -105,19 +105,28 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				// the standard favicon URI
 				"/favicon.ico",
 
-				// development-mode static resources
-				"/frontend/bower_components/**", "/icons/**", "/frontend/images/**", "/frontend/src/**", "/manifest.json",
+				// web application manifest
+				"/manifest.json",
 
-				// resources from the framework jars
-				"/*.html", "/*.js",
+				// icons
+				"/icons/**",
 
-				// development-mode webjars
+				// (development mode) static resources
+				"/frontend/bower_components/**",
+				"/frontend/images/**",
+				"/frontend/src/**",
+
+				// (development mode) resources from the framework jars
+				"/frontend/*.html",
+				"/frontend/*.js",
+
+				// (development mode) webjars
 				"/webjars/**",
 
-				//For debugging.
+				// (development mode) H2 debugging console
 				"/h2-console/**",
 
-				// production-mode static resources
-				"/frontend/build/**", "/frontend-es5/**", "/frontend-es6/**");
+				// (production mode) static resources
+				"/frontend/build/**");
 	}
 }

src/main/java/com/vaadin/starter/bakery/ui/view/login/LoginView.java
@@ -5,6 +5,9 @@ import com.vaadin.router.PageTitle;
 import com.vaadin.router.Route;
 import com.vaadin.router.event.AfterNavigationEvent;
 import com.vaadin.router.event.AfterNavigationObserver;
+import com.vaadin.server.InitialPageSettings;
+import com.vaadin.server.PageConfigurator;
+import com.vaadin.shared.ui.Dependency;
 import com.vaadin.ui.Tag;
 import com.vaadin.ui.common.HtmlImport;
 import com.vaadin.ui.polymertemplate.PolymerTemplate;
@@ -13,7 +16,17 @@ import com.vaadin.ui.polymertemplate.PolymerTemplate;
 @HtmlImport("src/login/bakery-login.html")
 @Route(value = "login")
 @PageTitle("###Bakery###")
-public class LoginView extends PolymerTemplate<LoginView.Model> implements AfterNavigationObserver {
+public class LoginView extends PolymerTemplate<LoginView.Model> implements PageConfigurator, AfterNavigationObserver {
+
+	@Override
+	public void configurePage(InitialPageSettings settings) {
+		// Force login page to use Shady DOM to avoid problems with browsers and
+		// password managers not supporting shadow DOM
+		settings.addInlineWithContents(InitialPageSettings.Position.PREPEND,
+				"window.customElements=window.customElements||{};" +
+						"window.customElements.forcePolyfill=true;" +
+						"window.ShadyDOM={force:true};", Dependency.Type.JAVASCRIPT);
+	}
 
 	@Override
 	public void afterNavigation(AfterNavigationEvent event) {

commit b16fbc8b687021d133077e462d61b36efc57368c

fix the background color on the Login View
regression from e8bf712 / PR #335

src/main/java/com/vaadin/starter/bakery/app/CustomBootstrapListener.java
@@ -29,8 +29,12 @@ public class CustomBootstrapListener implements BootstrapListener {
 		head.append(
 				"<meta name=\"viewport\" content=\"width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes\">");
 		head.append("<!-- Add any global styles for body, document, etc. -->\n" +
-                "    <custom-style>\n" +
-                "      <style is=\"custom-style\" include=\"valo-color valo-typography\"></style>\n" +
-                "    </custom-style>");
+				"    <custom-style>\n" +
+				"      <style is=\"custom-style\" include=\"valo-color valo-typography\">\n" +
+				"        html {\n" +
+				"          background: var(--valo-shade-10pct);\n" +
+				"        }\n" +
+				"      </style>\n" +
+				"    </custom-style>");
 	}
 }

src/main/webapp/frontend/src/app/bakery-app.html
@@ -11,7 +11,6 @@
         height: 100%;
         display: flex;
         flex-direction: column-reverse;
-        background: var(--valo-shade-10pct);
         color: var(--valo-body-text-color);
         font-family: var(--valo-font-family);
       }