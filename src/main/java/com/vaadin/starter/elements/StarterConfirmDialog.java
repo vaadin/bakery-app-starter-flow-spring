b0f8758de71fa7aa0d21937d362344b4ac5d7967
Manolo Carrasco <manolo@apache.org>
2017-12-18 07:17:16 +0100

    Remove getters, license.

--- src/main/java/com/vaadin/starter/elements/StarterConfirmDialog.java
+++ src/main/java/com/vaadin/starter/elements/StarterConfirmDialog.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2000-2017 Vaadin Ltd.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 package com.vaadin.starter.elements;
 
 import com.vaadin.shared.Registration;
@@ -25,8 +10,7 @@ import com.vaadin.ui.event.DomEvent;
 import com.vaadin.ui.event.Synchronize;
 
 /**
- * Java implementation of the polymer element
- * `starter-elements/starter-confirm-dialog`
+ * Java wrapper of the polymer element `starter-elements/starter-confirm-dialog`
  * 
  * TODO: this hand-written class should be replaced by an auto-generated one
  * when flow provides a maven plugin for generating java classes for 3rd party
@@ -44,24 +28,12 @@ public class StarterConfirmDialog extends Component {
 		getElement().setProperty("caption", caption == null ? "" : caption);
 	}
 
-	public String getMessage() {
-		return getElement().getProperty("message");
-	}
-
 	public void setMessage(String message) {
 		getElement().setProperty("message", message == null ? "" : message);
 	}
 
-	public String getOkText() {
-		return getElement().getProperty("okText");
-	}
-
 	public void setOkText(String okText) {
 		getElement().setProperty("okText", okText == null ? "" : okText);
-	}
-
-	public String getCancelText() {
-		return getElement().getProperty("cancelText");
 	}
 
 	public void setCancelText(String cancelText) {
@@ -84,10 +56,9 @@ public class StarterConfirmDialog extends Component {
 		}
 	}
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public Registration addCancelClickListener(
 			ComponentEventListener<CancelClickEvent> listener) {
-		return addListener(CancelClickEvent.class, (ComponentEventListener) listener);
+		return addListener(CancelClickEvent.class, listener);
 	}
 
 	@DomEvent("ok-click")
@@ -97,9 +68,8 @@ public class StarterConfirmDialog extends Component {
 		}
 	}
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public Registration addOkClickListener(
 			ComponentEventListener<OkClickEvent> listener) {
-		return addListener(OkClickEvent.class, (ComponentEventListener) listener);
+		return addListener(OkClickEvent.class, listener);
 	}
 }
