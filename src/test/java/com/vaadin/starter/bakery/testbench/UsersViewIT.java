03eb53e6d0020efc9911322a7d9eb913348779d3
Manolo Carrasco <manolo@apache.org>
2018-01-03 11:49:27 +0100

    Fix and check password validator in test

--- src/test/java/com/vaadin/starter/bakery/testbench/UsersViewIT.java
+++ src/test/java/com/vaadin/starter/bakery/testbench/UsersViewIT.java
@@ -1,6 +1,7 @@
 package com.vaadin.starter.bakery.testbench;
 
 import static com.vaadin.starter.bakery.backend.service.UserService.MODIFY_LOCKED_USER_NOT_PERMITTED;
+import static com.vaadin.starter.bakery.ui.view.EntityPresenter.REQUIRED_MESSAGE;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,7 +10,6 @@ import org.openqa.selenium.WebElement;
 import com.vaadin.starter.bakery.testbench.elements.core.FormLayoutElement;
 import com.vaadin.starter.bakery.testbench.elements.core.PaperToastElement;
 import com.vaadin.starter.bakery.testbench.elements.core.PasswordFieldElement;
-import com.vaadin.starter.bakery.testbench.elements.core.TextFieldElement;
 import com.vaadin.starter.bakery.testbench.elements.ui.StorefrontViewElement;
 import com.vaadin.starter.bakery.testbench.elements.ui.UsersViewElement;
 
@@ -39,10 +39,19 @@ public class UsersViewIT extends AbstractIT {
 		PasswordFieldElement password = usersView.getPasswordField();
 		Assert.assertEquals("", password.getValue());
 
-		password.setValue("foobar");
+		// Invalid password prevents closing form
+		password.setValue("123");
+		usersView.getButtonsBar().getSaveButton().click();
+		PaperToastElement toast = $(PaperToastElement.class).onPage().id("_defaultToast");
+		Assert.assertTrue(form.isDisplayed());
+		Assert.assertEquals(REQUIRED_MESSAGE, toast.getText());
+
+		// Good password
+		password.setValue("Abc123");
 		usersView.getButtonsBar().getSaveButton().click();
 		Assert.assertFalse(form.isDisplayed());
 
+		// Do not display any password when opening the form
 		bakerCell.click();
 		Assert.assertEquals("", password.getAttribute("value"));
 	}
@@ -53,11 +62,12 @@ public class UsersViewIT extends AbstractIT {
 
 		page.getGridCell("barista@vaadin.com").click();
 
-		TextFieldElement field = page.getFirstField();
-		field.setValue(field.getValue() + "-updated");
+		PasswordFieldElement field = page.getPasswordField();
+		field.setValue("Abc123");
 		page.getButtonsBar().getSaveButton().click();
 
 		PaperToastElement toast = $(PaperToastElement.class).onPage().id("_persistentToast");
+
 		Assert.assertEquals(MODIFY_LOCKED_USER_NOT_PERMITTED, toast.getText());
 		Assert.assertTrue(toast.isOpened());
 	}
