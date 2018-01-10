45c3a57dca883b37c32cb3c82c44f82df7df3002
Manolo Carrasco <manolo@apache.org>
2018-01-10 17:05:27 +0100

    Addapt tests for vaadin-tabs

--- src/test/java/com/vaadin/starter/bakery/testbench/elements/core/PaperTabElement.java
+++ src/test/java/com/vaadin/starter/bakery/testbench/elements/core/PaperTabElement.java
@@ -3,7 +3,7 @@ package com.vaadin.starter.bakery.testbench.elements.core;
 import com.vaadin.testbench.TestBenchElement;
 import com.vaadin.testbench.elementsbase.Element;
 
-@Element("paper-tab")
+@Element("vaadin-tab")
 public class PaperTabElement extends TestBenchElement {
 
 }
