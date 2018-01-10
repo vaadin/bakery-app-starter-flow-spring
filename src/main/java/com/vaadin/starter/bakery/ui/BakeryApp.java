9fc67c559fe94470a51898c7f3bc7194bf6bda3c
Manolo Carrasco <manolo@apache.org>
2018-01-10 11:17:01 +0100

    Fix select tab based on location when reloading app

--- src/main/java/com/vaadin/starter/bakery/ui/BakeryApp.java
+++ src/main/java/com/vaadin/starter/bakery/ui/BakeryApp.java
@@ -30,6 +30,6 @@ public class BakeryApp extends PolymerTemplate<TemplateModel>
 
 	@Override
 	public void afterNavigation(AfterNavigationEvent event) {
-		navigation.onLocationChange(event.getLocation().getPath());
+		navigation.onLocationChange(event.getLocation().getFirstSegment());
 	}
 }
