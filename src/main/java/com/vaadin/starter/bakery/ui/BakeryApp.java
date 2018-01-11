5f2a01fe413559113717378f4421de74749f37e2
Manolo Carrasco <manolo@apache.org>
2018-01-11 09:43:11 +0100

    Set default page when empty fragment

--- src/main/java/com/vaadin/starter/bakery/ui/BakeryApp.java
+++ src/main/java/com/vaadin/starter/bakery/ui/BakeryApp.java
@@ -8,6 +8,7 @@ import com.vaadin.router.event.BeforeNavigationEvent;
 import com.vaadin.router.event.BeforeNavigationObserver;
 import com.vaadin.starter.bakery.app.security.SecurityUtils;
 import com.vaadin.starter.bakery.ui.exceptions.AccessDeniedException;
+import com.vaadin.starter.bakery.ui.utils.BakeryConst;
 import com.vaadin.ui.Tag;
 import com.vaadin.ui.common.HtmlImport;
 import com.vaadin.ui.polymertemplate.Id;
@@ -30,6 +31,8 @@ public class BakeryApp extends PolymerTemplate<TemplateModel>
 
 	@Override
 	public void afterNavigation(AfterNavigationEvent event) {
-		navigation.onLocationChange(event.getLocation().getFirstSegment());
+		String currentPath = event.getLocation().getFirstSegment().isEmpty() ? BakeryConst.PAGE_DEFAULT
+				: event.getLocation().getFirstSegment();
+		navigation.onLocationChange(currentPath);
 	}
 }
