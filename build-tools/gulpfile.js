8921ee72ba1edcfc93b5c15b4f0eba7ab97bbcbb
Tulio Garcia <tulio@vaadin.com>
2018-01-26 11:57:02 +0200

    Restored build-tools for linting and fixed unit tests.

---
+++ build-tools/gulpfile.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+var htmlExtract = require('gulp-html-extract');
+var stylelint = require('gulp-stylelint');
+
+gulp.task('lint', ['lint:js', 'lint:html', 'lint:css']);
+
+gulp.task('lint:js', function() {
+  return gulp.src([
+    '../src/main/webapp/frontend/*.js',
+    '../src/main/webapp/frontend/src/**/*.js',
+    '../src/main/webapp/frontend/test/**/*.js'
+  ])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError('fail'));
+});
+
+gulp.task('lint:html', function() {
+  return gulp.src([
+    '../src/main/webapp/frontend/*.html',
+    '../src/main/webapp/frontend/src/**/*.html',
+    '../src/main/webapp/frontend/test/**/*.html'
+  ])
+    .pipe(htmlExtract({
+      sel: 'script, code-example code',
+      strip: true
+    }))
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError('fail'));
+});
+
+gulp.task('lint:css', function() {
+  return gulp.src([
+    '../src/main/webapp/frontend/*.html',
+    '../src/main/webapp/frontend/src/**/*.html',
+    '../src/main/webapp/frontend/test/**/*.html'
+  ])
+    .pipe(htmlExtract({
+      sel: 'style'
+    }))
+    .pipe(stylelint({
+      reporters: [
+        {formatter: 'string', console: true}
+      ]
+    }));
+});
--- src/test/java/com/vaadin/starter/bakery/testbench/elements/core/PaperToastElement.java
+++ src/test/java/com/vaadin/starter/bakery/testbench/elements/core/PaperToastElement.java
@@ -1,16 +1,18 @@
 package com.vaadin.starter.bakery.testbench.elements.core;
 
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
 import com.vaadin.testbench.TestBenchElement;
 import com.vaadin.testbench.elementsbase.Element;
 
 @Element("paper-toast")
 public class PaperToastElement extends TestBenchElement {
 
-	@Override
-	public String getText() {
+	public String getMessage() {
+		waitUntil(ExpectedConditions.attributeToBeNotEmpty(this, "text"));
 		return getPropertyString("text");
 	}
-
+	
 	public Boolean isOpened() {
 		return getPropertyBoolean("opened");
 	}
