724333f222fbe969759ac9205a7fad1c69125413
Manuel Carrasco Moñino <manolo@apache.org>
2018-02-01 13:58:42 +0100

    Remove set-value workaround from combo-box and datepicker. (#414)
    * Remove set-value workaround from combo-box and datepicker.
    
    Jira: BFF-425
    
    * Merge branch 'master' into fix/425-remove-value-workaround

--- src/main/java/com/vaadin/starter/bakery/ui/components/ComboBoxForBinder.java
+++ src/main/java/com/vaadin/starter/bakery/ui/components/ComboBoxForBinder.java
@@ -5,14 +5,9 @@ import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.shared.Registration;
 
 /**
- * Workaround for date-picker to:
- * 
- * 1.- set the initial value. Might be related with setValue not working when
- * element is not displayed https://github.com/vaadin/flow/issues/3351
- * 
- * 2.- avoid receiving a value-changed event when the value has been changed
- * from server side. This prevents binder marking the bean dirty.
- * https://github.com/vaadin/flow/issues/3350
+ * Workaround for date-picker to avoid receiving a value-changed event when the
+ * value has been changed from server side. This prevents binder marking the
+ * bean dirty. https://github.com/vaadin/flow/issues/3350
  */
 public class ComboBoxForBinder<T> extends ComboBox<T> implements Focusable<ComboBox<T>> {
 
@@ -22,9 +17,6 @@ public class ComboBoxForBinder<T> extends ComboBox<T> implements Focusable<Combo
 	public void setValue(T value) {
 		super.setValue(value);
 		this.lastSetValue = value;
-		if (value != null) {
-			getElement().setAttribute("value", getItemLabelGenerator().apply(value));
-		}
 	}
 
 	@Override
