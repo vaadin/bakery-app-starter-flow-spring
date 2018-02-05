7a65318358b1331815dfb0870d9b34d84d7666ed
Alex <alex@vaadin.com>
2018-02-05 11:58:50 +0200

    Explicitly define lumo themed elements
    Remove elements which are bound by id since their theming is handled by flow

--- src/main/java/com/vaadin/starter/bakery/ui/views/login/LoginView.java
+++ src/main/java/com/vaadin/starter/bakery/ui/views/login/LoginView.java
@@ -16,9 +16,9 @@ import com.vaadin.flow.theme.lumo.Lumo;
 @Tag("login-view")
 @HtmlImport("src/views/login/login-view.html")
 // Override elements with Lumo styled ones
-@HtmlImport("bower_components/vaadin-button/vaadin-button.html")
-@HtmlImport("bower_components/vaadin-text-field/vaadin-text-field.html")
-@HtmlImport("bower_components/vaadin-text-field/vaadin-password-field.html")
+@HtmlImport("bower_components/vaadin-button/theme/lumo/vaadin-button.html")
+@HtmlImport("bower_components/vaadin-text-field/theme/lumo/vaadin-text-field.html")
+@HtmlImport("bower_components/vaadin-text-field/theme/lumo/vaadin-password-field.html")
 @Route(value = "login")
 @PageTitle("###Bakery###")
 @Theme(Lumo.class)
