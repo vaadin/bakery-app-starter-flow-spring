62ab9c1b8293945865cc84c1dd5a815db61eb982
Manolo Carrasco <manolo@apache.org>
2018-02-06 18:27:57 +0100

    Make spring create and configure user and order editors

--- src/main/java/com/vaadin/starter/bakery/ui/views/admin/users/UserForm.java
+++ src/main/java/com/vaadin/starter/bakery/ui/views/admin/users/UserForm.java
@@ -1,5 +1,7 @@
 package com.vaadin.starter.bakery.ui.views.admin.users;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.vaadin.flow.component.HasText;
@@ -13,6 +15,7 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.provider.DataProvider;
 import com.vaadin.flow.data.provider.ListDataProvider;
+import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.templatemodel.TemplateModel;
 import com.vaadin.starter.bakery.backend.data.Role;
 import com.vaadin.starter.bakery.backend.data.entity.User;
@@ -22,10 +25,12 @@ import com.vaadin.starter.bakery.ui.crud.CrudView.CrudForm;
 
 @Tag("user-form")
 @HtmlImport("src/views/admin/users/user-form.html")
+@SpringComponent
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class UserForm extends PolymerTemplate<TemplateModel> implements CrudForm<User> {
-	
+
 	@Id("title")
-	private H3 title;	
+	private H3 title;
 
 	@Id("buttons")
 	private FormButtonsBar buttons;
@@ -45,9 +50,9 @@ public class UserForm extends PolymerTemplate<TemplateModel> implements CrudForm
 	@Id("role")
 	private ComboBoxForBinder<String> roleField;
 
-	private PasswordEncoder passwordEncoder;
+	private final PasswordEncoder passwordEncoder;
 
-	void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+	public UserForm(PasswordEncoder passwordEncoder) {
 		this.passwordEncoder = passwordEncoder;
 	}
 
@@ -56,7 +61,7 @@ public class UserForm extends PolymerTemplate<TemplateModel> implements CrudForm
 		ListDataProvider<String> roleProvider = DataProvider.ofItems(Role.getAllRoles());
 		roleField.setItemLabelGenerator(s -> s != null ? s : "");
 		roleField.setDataProvider(roleProvider);
-		
+
 		binder.bind(firstnameField, "firstName");
 		binder.bind(lastnameField, "lastName");
 		binder.bind(emailField, "email");
@@ -65,11 +70,9 @@ public class UserForm extends PolymerTemplate<TemplateModel> implements CrudForm
 		binder.forField(passwordField).withValidator(password -> {
 			return password.matches("^(|(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,})$");
 		}, "need 6 or more chars, mixing digits, lowercase and uppercase letters")
-				.bind(user -> passwordField.getEmptyValue(), (user, password) -> {
-					if (!passwordField.getEmptyValue().equals(password)) {
-						// user.
-						// user.setPassword(""); //
-						// passwordEncoder.encode(password));
+				.bind(user -> passwordField.getEmptyValue(), (user, pass) -> {
+					if (!passwordField.getEmptyValue().equals(pass)) {
+						user.setPassword(passwordEncoder.encode(pass));
 					}
 				});
 	}
