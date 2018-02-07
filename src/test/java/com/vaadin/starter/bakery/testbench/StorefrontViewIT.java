7b9e251eeec848477492a3817ab798383019b9a5
Johannes Häyry <johannes.hayry@vaadin.com>
2018-02-07 17:39:37 +0200

    Fixed test, but noticed an earlier regression
    - Marked test ignored until regression fixed

--- src/test/java/com/vaadin/starter/bakery/testbench/StorefrontViewIT.java
+++ src/test/java/com/vaadin/starter/bakery/testbench/StorefrontViewIT.java
@@ -1,10 +1,10 @@
 package com.vaadin.starter.bakery.testbench;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.vaadin.flow.component.button.testbench.ButtonElement;
-import com.vaadin.flow.component.grid.testbench.GridElement;
 import com.vaadin.starter.bakery.testbench.elements.components.OrderCardElement;
 import com.vaadin.starter.bakery.testbench.elements.ui.StorefrontViewElement;
 
@@ -17,28 +17,15 @@ public class StorefrontViewIT extends AbstractIT {
 	}
 
 	@Test
-	public void orderCardExpandAndCollapse() {
-		StorefrontViewElement storefrontPage = openStorefrontPage();
-
-		GridElement grid = storefrontPage.getGrid();
-		Assert.assertTrue(grid.getRowCount() >= 0);
-
-		OrderCardElement firstOrder = storefrontPage.getFirstOrderCard();
-		Assert.assertNotNull(firstOrder);
-		Assert.assertFalse(firstOrder.isOrderSelected());
-
-		firstOrder.click();
-		Assert.assertTrue(firstOrder.isOrderSelected());
-	}
-
-	@Test
+	@Ignore("BFF-614")
 	public void editOrder() {
 		StorefrontViewElement storefrontPage = openStorefrontPage();
 
 		OrderCardElement firstOrder = storefrontPage.getOrderCard(0);
 		Assert.assertNotNull(firstOrder);
 		firstOrder.click();
-		ButtonElement editBtn = firstOrder.getDetail().getEditButton();
+
+		ButtonElement editBtn = storefrontPage.getOrderDetails().getEditButton();
 		editBtn.click();
 
 		Assert.assertThat(getDriver().getCurrentUrl(), endsWith("edit="));
--- src/test/java/com/vaadin/starter/bakery/testbench/elements/ui/StorefrontViewElement.java
+++ src/test/java/com/vaadin/starter/bakery/testbench/elements/ui/StorefrontViewElement.java
@@ -2,6 +2,7 @@ package com.vaadin.starter.bakery.testbench.elements.ui;
 
 import com.vaadin.flow.component.grid.testbench.GridElement;
 import com.vaadin.starter.bakery.testbench.elements.components.OrderCardElement;
+import com.vaadin.starter.bakery.testbench.elements.components.OrderDetailsFullElement;
 import com.vaadin.testbench.TestBenchElement;
 import com.vaadin.testbench.elementsbase.Element;
 
@@ -19,5 +20,9 @@ public class StorefrontViewElement extends TestBenchElement implements HasApp, H
 
 	public OrderCardElement getOrderCard(int index) {
 		return getGrid().$(OrderCardElement.class).get(index);
+	}
+
+	public OrderDetailsFullElement getOrderDetails() {
+		return getFormDialog().$(OrderDetailsFullElement.class).first();
 	}
 }
