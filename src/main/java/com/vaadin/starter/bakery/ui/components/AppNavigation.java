49e3aa824f013465baaa049923a0759c353a04c6
Manolo Carrasco <manolo@apache.org>
2018-02-09 16:08:37 +0100

    Use vaadin-tabs-flow in java instead of dom-repeat in template
    Jira: BFF-536
    
    I tried not to change all the implementation, but just handle tab.selected
    property from server side by bounding `vaadin-tabs` via the `@Id` annotation
    but I come with the bug in flow reported at
    https://github.com/vaadin/flow/issues/3530
    
    Hence I did all the work to remove client side logic.

--- src/main/java/com/vaadin/starter/bakery/ui/components/AppNavigation.java
+++ src/main/java/com/vaadin/starter/bakery/ui/components/AppNavigation.java
@@ -1,12 +1,18 @@
 package com.vaadin.starter.bakery.ui.components;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import com.vaadin.flow.component.ClientDelegate;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.IronIcon;
+import com.vaadin.flow.component.polymertemplate.Id;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.AfterNavigationEvent;
 import com.vaadin.flow.router.AfterNavigationObserver;
 import com.vaadin.flow.templatemodel.TemplateModel;
@@ -17,42 +23,43 @@ import com.vaadin.starter.bakery.ui.entities.PageInfo;
 public class AppNavigation extends PolymerTemplate<AppNavigation.Model> implements AfterNavigationObserver {
 
 	public interface Model extends TemplateModel {
-		void setPageNumber(int page);
-
-		void setPages(List<PageInfo> pages);
 	}
 
-	private List<PageInfo> pages;
 	private String logoutHref;
 	private String defaultHref;
 
+	@Id("tabs")
+	private Tabs tabs;
+
+	private List<String> links = new ArrayList<>();
+
 	public void init(List<PageInfo> pages, String defaultHref, String logoutHref) {
-		this.pages = pages;
-		this.defaultHref = defaultHref;
-		this.logoutHref = logoutHref;
-		getModel().setPages(pages);
+		for (PageInfo page : pages) {
+			Tab tab = new Tab(new Div(new IronIcon("vaadin", page.getIcon()), new Span(page.getTitle())));
+			links.add(page.getLink());
+			tabs.add(tab);
+		}
+
+		tabs.addSelectedChangeListener(e -> navigate());
 	}
 
-	@ClientDelegate
-	private void navigateTo(String href) {
-		if (href.equals(logoutHref)) {
-			// The logout button is a 'normal' URL, not Flow-managed but
-			// handled by Spring Security.
-			UI.getCurrent().getPage().executeJavaScript("location.assign('logout')");
-		} else {
-			UI.getCurrent().navigateTo(href);
+	private void navigate() {
+		String href = links.get(tabs.getSelectedIndex());
+		if (href != null) {
+			if (href.equals(logoutHref)) {
+				// The logout button is a 'normal' URL, not Flow-managed but
+				// handled by Spring Security.
+				UI.getCurrent().getPage().executeJavaScript("location.assign('logout')");
+			} else {
+				UI.getCurrent().navigateTo(href);
+			}
 		}
 	}
 
 	@Override
 	public void afterNavigation(AfterNavigationEvent event) {
-		String currentPath = event.getLocation().getFirstSegment().isEmpty() ? defaultHref
+		String path = event.getLocation().getFirstSegment().isEmpty() ? defaultHref
 				: event.getLocation().getFirstSegment();
-
-		for (int i = 0; i < pages.size(); i++) {
-			if (pages.get(i).getLink().equals(currentPath)) {
-				this.getModel().setPageNumber(i);
-			}
-		}
+		tabs.setSelectedIndex(links.indexOf(path));
 	}
 }
