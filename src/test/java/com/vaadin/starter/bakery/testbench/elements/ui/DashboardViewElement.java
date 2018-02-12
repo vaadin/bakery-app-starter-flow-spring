5f6c0edb04b40c8207adaac0137c62e1c1ca514e
Artur <artur@vaadin.com>
2018-02-12 22:40:24 +0200

    Use TestBench instead of the separate parts (#443)
    * Use TestBench instead of the separate parts

--- src/test/java/com/vaadin/starter/bakery/testbench/elements/ui/DashboardViewElement.java
+++ src/test/java/com/vaadin/starter/bakery/testbench/elements/ui/DashboardViewElement.java
@@ -1,14 +1,14 @@
 package com.vaadin.starter.bakery.testbench.elements.ui;
 
-import com.vaadin.starter.bakery.testbench.elements.core.VaadinBoardElement;
+import com.vaadin.flow.component.board.testbench.BoardElement;
 import com.vaadin.testbench.TestBenchElement;
 import com.vaadin.testbench.elementsbase.Element;
 
 @Element("dashboard-view")
 public class DashboardViewElement extends TestBenchElement implements HasApp {
 
-	public VaadinBoardElement getBoard() {
-		return $(VaadinBoardElement.class).first();
+	public BoardElement getBoard() {
+		return $(BoardElement.class).first();
 	}
 
 }
