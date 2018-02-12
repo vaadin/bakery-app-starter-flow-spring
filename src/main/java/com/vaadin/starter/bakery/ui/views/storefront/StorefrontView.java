09e56ffd41eef93abd3e5aef00bfbb4fca9f2ea6
tuliogarcia <tulio@vaadin.com>
2018-02-12 11:33:02 +0200

    Change storefront url when dialog is closed (#434)
    * Cancel even if dirty.
    
    Jira: BFF-617

--- src/main/java/com/vaadin/starter/bakery/ui/views/storefront/StorefrontView.java
+++ src/main/java/com/vaadin/starter/bakery/ui/views/storefront/StorefrontView.java
@@ -115,7 +115,7 @@ public class StorefrontView extends PolymerTemplate<StorefrontView.Model>
 		getOpenedOrderDetails().addCancelListener(e -> presenter.cancel());
 
 		dialog.getElement().addEventListener("opened-changed", e -> {
-			if (!dialog.isOpened() && this.isDirty()) {
+			if (!dialog.isOpened()) {
 				// Handle client-side closing dialog on escape
 				presenter.cancel();
 			}
