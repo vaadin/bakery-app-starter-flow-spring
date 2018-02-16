48d258bb98264a04a6e1bdf6e623e562001456e9
Manolo Carrasco <manolo@apache.org>
2018-02-16 17:53:15 +0100

    Remove full suffix from order-details-full

--- src/main/java/com/vaadin/starter/bakery/ui/views/storefront/events/CommentEvent.java
+++ src/main/java/com/vaadin/starter/bakery/ui/views/storefront/events/CommentEvent.java
@@ -1,14 +1,14 @@
 package com.vaadin.starter.bakery.ui.views.storefront.events;
 
 import com.vaadin.flow.component.ComponentEvent;
-import com.vaadin.starter.bakery.ui.views.storefront.OrderDetailsFull;
+import com.vaadin.starter.bakery.ui.views.storefront.OrderDetails;
 
-public class CommentEvent extends ComponentEvent<OrderDetailsFull> {
+public class CommentEvent extends ComponentEvent<OrderDetails> {
 
 	private Long orderId;
 	private String message;
 
-	public CommentEvent(OrderDetailsFull component, Long orderId, String message) {
+	public CommentEvent(OrderDetails component, Long orderId, String message) {
 		super(component, false);
 		this.orderId = orderId;
 		this.message = message;
