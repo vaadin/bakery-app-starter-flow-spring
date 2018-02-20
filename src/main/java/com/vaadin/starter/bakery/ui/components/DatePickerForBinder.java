c8e81b2d1b342a145b8e42066dbd106a6ae1c273
Manuel Carrasco Moñino <manolo@apache.org>
2018-02-20 10:53:39 +0100

    Fix random duedate `invalid` property (#462)
    * workaround for invalid date attr not being notified to server side.
    
    It is necessary to fix the call to `validate` method in date-picker when
    it is dettached when it is already closed, see issue:
    https://github.com/vaadin/vaadin-date-picker/issues/513
    
    Additionally discovered a problem in dialog-flow which makes overlay
    children be disconnected form server side when dialog is closed, see issue:
    https://github.com/vaadin/vaadin-dialog-flow/issues/40
    
    Jira: BFF-639

--- src/main/java/com/vaadin/starter/bakery/ui/components/DatePickerForBinder.java
+++ src/main/java/com/vaadin/starter/bakery/ui/components/DatePickerForBinder.java
@@ -1,36 +1,16 @@
 package com.vaadin.starter.bakery.ui.components;
 
 
-import java.time.LocalDate;
-
+import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.datepicker.DatePicker;
-import com.vaadin.flow.shared.Registration;
+import com.vaadin.flow.component.dependency.HtmlImport;
 
 /**
- * Workaround for date-picker to avoid receiving a value-changed event when the
- * value has been changed from server side. This prevents binder marking the
- * bean dirty. https://github.com/vaadin/flow/issues/3350
+ * date-picker-for-binder extends vaadin-date-picker in order to override the
+ * `detached` method that causes problems by updating the invalid attribute when
+ * date-picker is inside a dialog and it is closed.
  */
+@Tag("date-picker-for-binder")
+@HtmlImport("src/components/date-picker-for-binder.html")
 public class DatePickerForBinder extends DatePicker {
-
-	private LocalDate lastNotifiedValue;
-	private boolean setFromServer;
-
-	@Override
-	public void setValue(LocalDate value) {
-		super.setValue(value);
-		setFromServer = true;
-	}
-
-	@Override
-	public Registration addValueChangeListener(ValueChangeListener<DatePicker, LocalDate> listener) {
-		return super.addValueChangeListener(e -> {
-			if (!setFromServer && (e.getValue() != null && !e.getValue().equals(lastNotifiedValue)
-					|| e.getValue() == null && lastNotifiedValue != null)) {
-				listener.onComponentEvent(e);
-			}
-			lastNotifiedValue = e.getValue();
-			setFromServer = false;
-		});
-	}
 }
