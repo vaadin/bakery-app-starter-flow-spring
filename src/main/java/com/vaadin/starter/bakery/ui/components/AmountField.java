1127cea71adbd7362773eac366115c3cc26b95c1
Manolo Carrasco <manolo@apache.org>
2018-02-22 09:13:41 +0100

    value needs to be synchronized

--- src/main/java/com/vaadin/starter/bakery/ui/components/AmountField.java
+++ src/main/java/com/vaadin/starter/bakery/ui/components/AmountField.java
@@ -2,6 +2,7 @@ package com.vaadin.starter.bakery.ui.components;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.HasValue;
+import com.vaadin.flow.component.Synchronize;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.HtmlImport;
 
@@ -11,32 +12,38 @@ public class AmountField extends Component implements HasValue<AmountField, Inte
 
 	@Override
 	public void setValue(Integer value) {
-		this.getElement().setProperty("value", value);
+		getElement().setProperty("value", value);
 	}
 
 	@Override
+	@Synchronize("value-changed")
 	public Integer getValue() {
-		String val = this.getElement().getProperty("value");
-		return val == null ? 0 : Integer.parseInt(val);
+		String val = getElement().getProperty("value");
+		try {
+			return Integer.parseInt(val);
+		} catch (NumberFormatException e) {
+			return 0;
+		}
 	}
 
 	public void setEnabled(boolean enabled) {
-		this.getElement().setProperty("disabled", !enabled);
+		getElement().setProperty("disabled", !enabled);
 	}
 
 	public void setMin(int value) {
-		this.getElement().setProperty("min", value);
+		getElement().setProperty("min", value);
 	}
 
 	public void setMax(int value) {
-		this.getElement().setProperty("max", value);
+		getElement().setProperty("max", value);
 	}
 
 	public void setEditable(boolean editable) {
-		this.getElement().setProperty("editable", editable);
+		getElement().setProperty("editable", editable);
 	}
 
 	public void setPattern(String pattern) {
-		this.getElement().setProperty("pattern", pattern);
+		getElement().setProperty("pattern", pattern);
 	}
+
 }
