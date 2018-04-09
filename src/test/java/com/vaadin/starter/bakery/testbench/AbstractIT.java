8dea52dac35eb396d117085ec5c7d6b5171ee246
Johannes Häyry <johannes.hayry@vaadin.com>
2018-04-09 15:40:28 +0300

    Add profile and use features for SauceLabs based testing (#544)
    * Modified tests to use TestBench SauceLabs helpers
    
    * Downgrade back to beta3 of platform
    
    * Fix mistake with TB dependency
    
    * Fix TB dependency
    
    * set sauce properties for failsafe (#543)
    
    * Add gridBrowsers piping to failsafe

--- src/test/java/com/vaadin/starter/bakery/testbench/AbstractIT.java
+++ src/test/java/com/vaadin/starter/bakery/testbench/AbstractIT.java
@@ -1,24 +1,17 @@
 package com.vaadin.starter.bakery.testbench;
 
-import com.vaadin.testbench.IPAddress;
-import com.vaadin.testbench.annotations.BrowserConfiguration;
-import com.vaadin.testbench.parallel.Browser;
-import com.vaadin.testbench.parallel.ParallelTest;
 import org.junit.Rule;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.slf4j.LoggerFactory;
 
 import com.vaadin.starter.bakery.testbench.elements.ui.LoginViewElement;
+import com.vaadin.testbench.IPAddress;
 import com.vaadin.testbench.ScreenshotOnFailureRule;
 import com.vaadin.testbench.TestBenchDriverProxy;
+import com.vaadin.testbench.parallel.ParallelTest;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
 
 public abstract class AbstractIT extends ParallelTest {
 
@@ -39,7 +32,6 @@ public abstract class AbstractIT extends ParallelTest {
 		if (getRunLocallyBrowser() == null) {
 			APP_URL = "http://" + IPAddress.findSiteLocalAddress() + ":8080/";
 		}
-		getCommandExecutor().resizeViewPortTo(1024, 768);
 	}
 
 	@Override
@@ -54,22 +46,5 @@ public abstract class AbstractIT extends ParallelTest {
 	protected LoginViewElement openLoginView(WebDriver driver, String url) {
 		driver.get(url);
 		return $(LoginViewElement.class).waitForFirst();
-	}
-
-	// Is needed for running tests on hub
-	@BrowserConfiguration
-	public List<DesiredCapabilities> getBrowsersToTest() {
-		List<DesiredCapabilities> allBrowsers = new ArrayList<>();
-		String browsers = System.getProperty("test.hub.browsers");
-		if (browsers != null) {
-			for (String browserName : browsers.split(",")) {
-				Browser browser = Browser.valueOf(
-						browserName.toUpperCase(Locale.ENGLISH).trim());
-				allBrowsers.add(browser.getDesiredCapabilities());
-			}
-		} else {
-			allBrowsers.add(Browser.CHROME.getDesiredCapabilities());
-		}
-		return allBrowsers;
 	}
 }
