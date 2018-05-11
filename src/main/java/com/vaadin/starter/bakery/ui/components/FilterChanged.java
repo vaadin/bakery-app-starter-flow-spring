4f1d06f8f639b8b492af37474dd41c910d042519
Tulio Garcia <tulio@vaadin.com>
2018-05-11 14:14:51 +0300

    Moved events to inner classes.

--- src/main/java/com/vaadin/starter/bakery/ui/components/FilterChanged.java
+++
@@ -1,9 +0,0 @@
-package com.vaadin.starter.bakery.ui.components;
-
-import com.vaadin.flow.component.ComponentEvent;
-
-public class FilterChanged extends ComponentEvent<SearchBar> {
-	public FilterChanged(SearchBar source, boolean fromClient) {
-		super(source, fromClient);
-	}
-}
--- src/main/java/com/vaadin/starter/bakery/ui/components/SearchBar.java
+++ src/main/java/com/vaadin/starter/bakery/ui/components/SearchBar.java
@@ -1,7 +1,10 @@
 package com.vaadin.starter.bakery.ui.components;
 
 import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.ComponentEvent;
 import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.DebounceSettings;
+import com.vaadin.flow.component.DomEvent;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.HtmlImport;
@@ -9,6 +12,7 @@ import com.vaadin.flow.component.polymertemplate.Id;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.dom.DebouncePhase;
 import com.vaadin.flow.templatemodel.TemplateModel;
 
 @Tag("search-bar")
@@ -71,5 +75,18 @@ public class SearchBar extends PolymerTemplate<SearchBar.Model> {
 
 	public void addActionClickListener(ComponentEventListener<ClickEvent<Button>> listener) {
 		actionButton.addClickListener(listener);
+	}
+
+	@DomEvent(value = "value-changed", debounce = @DebounceSettings(timeout = 300, phases = DebouncePhase.TRAILING))
+	public static class SearchValueChanged extends ComponentEvent<TextField> {
+		public SearchValueChanged(TextField source, boolean fromClient) {
+			super(source, fromClient);
+		}
+	}
+
+	public static class FilterChanged extends ComponentEvent<SearchBar> {
+		public FilterChanged(SearchBar source, boolean fromClient) {
+			super(source, fromClient);
+		}
 	}
 }
