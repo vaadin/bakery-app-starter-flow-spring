653077ed20c94b67209ddff2de7359d5d8ebc31d
Guillermo Alvarez <guillermo@vaadin.com>
2018-05-15 18:14:00 +0300

    Delete FlowComponentRendererElement

--- src/test/java/com/vaadin/starter/bakery/testbench/elements/core/FlowComponentRendererElement.java
+++
@@ -1,8 +0,0 @@
-package com.vaadin.starter.bakery.testbench.elements.core;
-
-import com.vaadin.testbench.TestBenchElement;
-import com.vaadin.testbench.elementsbase.Element;
-
-@Element("flow-component-renderer")
-public class FlowComponentRendererElement extends TestBenchElement {
-}
