6f24985ab744c62f9fd6bf1a9e2dfb84c1575203
Alex <alex@vaadin.com>
2018-06-25 11:34:20 +0300

    Fix ConfirmDialog Testbench element

--- src/test/java/com/vaadin/starter/bakery/testbench/elements/components/ConfirmDialogElement.java
+++ src/test/java/com/vaadin/starter/bakery/testbench/elements/components/ConfirmDialogElement.java
@@ -1,16 +1,21 @@
 package com.vaadin.starter.bakery.testbench.elements.components;
 
-import com.vaadin.flow.component.dialog.testbench.DialogElement;
+import com.vaadin.flow.component.button.testbench.ButtonElement;
+import com.vaadin.testbench.TestBenchElement;
 import com.vaadin.testbench.elementsbase.Element;
 
-@Element("confirm-dialog")
-public class ConfirmDialogElement extends DialogElement {
+@Element("vaadin-confirm-dialog")
+public class ConfirmDialogElement extends TestBenchElement {
+
+	private TestBenchElement getOverlayContext() {
+		return $("vaadin-dialog-overlay").onPage().last().$(TestBenchElement.class).id("content");
+	}
 
 	public void confirm() {
-		callFunction("_ok");
+		getOverlayContext().$(ButtonElement.class).id("confirm").click();
 	}
-	
+
 	public String getMessage() {
-		return getPropertyString("message");
+		return getOverlayContext().$(TestBenchElement.class).attribute("part", "message").first().getText();
 	}
 }
