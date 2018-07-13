11e0edec7cf4bf9c0259e5d68a92865378bde398
Tulio Garcia <tulio@vaadin.com>
2018-07-13 17:02:10 +0300

    Fixes from review

--- src/main/webapp/sw.js
+++ src/main/webapp/sw.js
@@ -1,18 +1,35 @@
+'use strict';
+
 // Important: update the version each time you change any of the files listed below
-var version = 1;
+const version = 5;
 // define your offline-page and assets used by it
-var offlinePage = 'offline-page.html';
-var offlineAssets = [offlinePage, 'manifest.json',
+const offlinePage = 'offline-page.html';
+const offlineAssets = [offlinePage, 'manifest.json',
   'images/offline-login-banner.jpg'
-]
+];
 
 const CACHE_NAME = 'static-v' + version;
 
-function updateCache() {
-  return caches.open(CACHE_NAME)
-    .then(function(cache) {
-      cache.addAll(offlineAssets);
-    });
+function createCacheBustedRequest(url) {
+  const request = new Request(url, {cache: 'reload'});
+  // See https://fetch.spec.whatwg.org/#concept-request-mode
+  // This is not yet supported in Chrome as of M48, so we need to explicitly check to see
+  // if the cache: 'reload' option had any effect.
+  if ('cache' in request) {
+    return request;
+  }
+
+  // If {cache: 'reload'} didn't have any effect, append a cache-busting URL parameter instead.
+  const bustedUrl = new URL(url, self.location.href);
+  bustedUrl.search += (bustedUrl.search ? '&' : '') + 'cachebust=' + Date.now();
+  return new Request(bustedUrl);
+}
+
+
+function cacheUrl(url) {
+  return fetch(createCacheBustedRequest(url))
+    .then(response => caches.open(CACHE_NAME)
+      .then(cache => cache.put(url, response)));
 }
 
 self.addEventListener('activate', event => {
@@ -23,7 +40,6 @@ self.addEventListener('activate', event => {
           if (CACHE_NAME !== cacheName) {
             // If this cache name isn't present in the array of "expected" cache names,
             // then delete it.
-            console.log('Deleting out of date cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
@@ -33,36 +49,32 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('install', function(event) {
-  event.waitUntil(updateCache());
+  event.waitUntil(Promise.all(
+    offlineAssets.map(url => cacheUrl(url))
+  ));
 });
 
-var doesRequestAcceptHtml = function(request) {
+const doesRequestAcceptHtml = function(request) {
   return request.headers.get('Accept')
     .split(',')
-    .some(function(type) {
-      return type === 'text/html';
-    });
+    .some(type => type === 'text/html');
 };
 
 self.addEventListener('fetch', function(event) {
-  var request = event.request;
+  const request = event.request;
   if (doesRequestAcceptHtml(request)) {
     // HTML pages fallback to offline page
     event.respondWith(
       fetch(request)
-        .catch(function() {
-          return caches.match(offlinePage);
-        })
+        .catch(() => caches.match(offlinePage))
     );
   } else {
     if (request.cache === 'only-if-cached' && request.mode !== 'same-origin') {
-        return;
+      return;
     }
     event.respondWith(
       caches.match(request)
-        .then(function(response) {
-          return response || fetch(request);
-        })
+        .then(response => response || fetch(request))
     );
   }
 });
