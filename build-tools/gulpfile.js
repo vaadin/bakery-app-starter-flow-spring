d5cc12c99ebd02cf5555f4ccc8096a522d86b7af
Guille <alvarezguille@users.noreply.github.com>
2018-07-24 17:22:50 +0300

    Revert "Move static files to resources directory (#635)"
    This reverts commit 9758aa70d59c37fca18947b4faf2cbf0c12c515b.

--- build-tools/gulpfile.js
+++ build-tools/gulpfile.js
@@ -10,10 +10,10 @@ gulp.task('lint', ['lint:js', 'lint:html', 'lint:css']);
 gulp.task('lint:js', function() {
   return gulp.src([
     'gulpfile.js',
-    '../src/main/resources/static/sw.js',
-    '../src/main/resources/static/frontend/*.js',
-    '../src/main/resources/static/frontend/src/**/*.js',
-    '../src/main/resources/static/frontend/test/**/*.js'
+    '../src/main/webapp/sw.js',
+    '../src/main/webapp/frontend/*.js',
+    '../src/main/webapp/frontend/src/**/*.js',
+    '../src/main/webapp/frontend/test/**/*.js'
   ])
     .pipe(eslint())
     .pipe(eslint.format())
@@ -22,11 +22,11 @@ gulp.task('lint:js', function() {
 
 gulp.task('lint:html', function() {
   return gulp.src([
-    '../src/main/resources/static/frontend/*.html',
-    '!../src/main/resources/static/frontend/flow-component-renderer.html',
-    '!../src/main/resources/static/frontend/flow-grid-component-renderer.html',
-    '../src/main/resources/static/frontend/src/**/*.html',
-    '../src/main/resources/static/frontend/test/**/*.html'
+    '../src/main/webapp/frontend/*.html',
+    '!../src/main/webapp/frontend/flow-component-renderer.html',
+    '!../src/main/webapp/frontend/flow-grid-component-renderer.html',
+    '../src/main/webapp/frontend/src/**/*.html',
+    '../src/main/webapp/frontend/test/**/*.html'
   ])
     .pipe(htmlExtract({
       sel: 'script, code-example code',
@@ -39,11 +39,11 @@ gulp.task('lint:html', function() {
 
 gulp.task('lint:css', function() {
   return gulp.src([
-    '../src/main/resources/static/frontend/*.html',
-    '!../src/main/resources/static/frontend/flow-component-renderer.html',
-    '!../src/main/resources/static/frontend/flow-grid-component-renderer.html',
-    '../src/main/resources/static/frontend/src/**/*.html',
-    '../src/main/resources/static/frontend/test/**/*.html'
+    '../src/main/webapp/frontend/*.html',
+    '!../src/main/webapp/frontend/flow-component-renderer.html',
+    '!../src/main/webapp/frontend/flow-grid-component-renderer.html',
+    '../src/main/webapp/frontend/src/**/*.html',
+    '../src/main/webapp/frontend/test/**/*.html'
   ])
     .pipe(htmlExtract({
       sel: 'style'
