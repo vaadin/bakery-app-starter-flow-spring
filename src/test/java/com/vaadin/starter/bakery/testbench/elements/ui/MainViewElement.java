1d11a979faf6a2af37e8a6f78aa5966f1e08384f
Alex <alex@vaadin.com>
2018-09-11 18:33:57 +0300

    Adjust new component with IT

--- src/test/java/com/vaadin/starter/bakery/testbench/elements/ui/MainViewElement.java
+++ src/test/java/com/vaadin/starter/bakery/testbench/elements/ui/MainViewElement.java
@@ -1,11 +1,9 @@
 package com.vaadin.starter.bakery.testbench.elements.ui;
 
+import com.vaadin.flow.component.applayout.testbench.AppLayoutElement;
 import com.vaadin.starter.bakery.testbench.elements.components.AppNavigationElement;
-import com.vaadin.testbench.TestBenchElement;
-import com.vaadin.testbench.elementsbase.Element;
 
-@Element("main-view")
-public class MainViewElement extends TestBenchElement {
+public class MainViewElement extends AppLayoutElement {
 
 	public AppNavigationElement getMenu() {
 		return $(AppNavigationElement.class).first();
