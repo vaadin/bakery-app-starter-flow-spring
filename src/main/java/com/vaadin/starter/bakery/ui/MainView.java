9c20cc70aed3be3b8656d7112126e25a79dac421
Alex <alex@vaadin.com>
2018-09-11 17:13:34 +0300

    Take into use vaadin-app-layout-flow
    To save existing functionality:
    Added offline banner component
    Moved search field focus/blur listeners to server-side

--- src/main/java/com/vaadin/starter/bakery/ui/MainView.java
+++ src/main/java/com/vaadin/starter/bakery/ui/MainView.java
@@ -1,95 +1,80 @@
 package com.vaadin.starter.bakery.ui;
 
-import static com.vaadin.starter.bakery.ui.utils.BakeryConst.ICON_DASHBOARD;
-import static com.vaadin.starter.bakery.ui.utils.BakeryConst.ICON_LOGOUT;
-import static com.vaadin.starter.bakery.ui.utils.BakeryConst.ICON_PRODUCTS;
-import static com.vaadin.starter.bakery.ui.utils.BakeryConst.ICON_STOREFRONT;
-import static com.vaadin.starter.bakery.ui.utils.BakeryConst.ICON_USERS;
-import static com.vaadin.starter.bakery.ui.utils.BakeryConst.PAGE_DASHBOARD;
-import static com.vaadin.starter.bakery.ui.utils.BakeryConst.PAGE_DEFAULT;
-import static com.vaadin.starter.bakery.ui.utils.BakeryConst.PAGE_LOGOUT;
-import static com.vaadin.starter.bakery.ui.utils.BakeryConst.PAGE_PRODUCTS;
-import static com.vaadin.starter.bakery.ui.utils.BakeryConst.PAGE_STOREFRONT;
-import static com.vaadin.starter.bakery.ui.utils.BakeryConst.PAGE_USERS;
-import static com.vaadin.starter.bakery.ui.utils.BakeryConst.TITLE_DASHBOARD;
-import static com.vaadin.starter.bakery.ui.utils.BakeryConst.TITLE_LOGOUT;
-import static com.vaadin.starter.bakery.ui.utils.BakeryConst.TITLE_PRODUCTS;
-import static com.vaadin.starter.bakery.ui.utils.BakeryConst.TITLE_STOREFRONT;
-import static com.vaadin.starter.bakery.ui.utils.BakeryConst.TITLE_USERS;
-import static com.vaadin.starter.bakery.ui.utils.BakeryConst.VIEWPORT;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.vaadin.flow.component.HasElement;
-import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.applayout.AbstractAppRouterLayout;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.RoutingMenuItem;
 import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
-import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.page.Viewport;
-import com.vaadin.flow.component.polymertemplate.Id;
-import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
-import com.vaadin.flow.router.PageTitle;
-import com.vaadin.flow.router.RouterLayout;
 import com.vaadin.flow.server.PWA;
-import com.vaadin.flow.templatemodel.TemplateModel;
 import com.vaadin.starter.bakery.app.security.SecurityUtils;
-import com.vaadin.starter.bakery.ui.components.AppNavigation;
 import com.vaadin.starter.bakery.ui.components.BakeryCookieConsent;
-import com.vaadin.starter.bakery.ui.entities.PageInfo;
+import com.vaadin.starter.bakery.ui.components.OfflineBanner;
 import com.vaadin.starter.bakery.ui.exceptions.AccessDeniedException;
 import com.vaadin.starter.bakery.ui.views.HasConfirmation;
 import com.vaadin.starter.bakery.ui.views.admin.products.ProductsView;
 import com.vaadin.starter.bakery.ui.views.admin.users.UsersView;
 import com.vaadin.starter.bakery.ui.views.login.LoginView;
 
-@Tag("main-view")
-@HtmlImport("src/main-view.html")
+import static com.vaadin.starter.bakery.ui.utils.BakeryConst.*;
 
-@PageTitle("###Bakery###")
+
 @Viewport(VIEWPORT)
 @PWA(name = "Bakery App Starter", shortName = "###Bakery###", startPath = "login",
 		backgroundColor = "#227aef", themeColor = "#227aef",
 		manifestPath = "manifest.json",
 		offlinePath = "offline-page.html", offlineResources =
 		{"images/offline-login-banner.jpg"})
-public class MainView extends PolymerTemplate<TemplateModel>
-		implements RouterLayout, BeforeEnterObserver {
-
-	@Id("appNavigation")
-	private AppNavigation appNavigation;
+public class MainView extends AbstractAppRouterLayout implements BeforeEnterObserver {
 
 	private final ConfirmDialog confirmDialog;
 
-	@Autowired
 	public MainView() {
 		this.confirmDialog = new ConfirmDialog();
 		confirmDialog.setCancelable(true);
 		confirmDialog.setConfirmButtonTheme("raised tertiary error");
 		confirmDialog.setCancelButtonTheme("raised tertiary");
 
-		List<PageInfo> pages = new ArrayList<>();
-		if (SecurityUtils.isUserLoggedIn()) {
-			pages.add(new PageInfo(PAGE_STOREFRONT, ICON_STOREFRONT,
-				TITLE_STOREFRONT));
-			pages.add(
-				new PageInfo(PAGE_DASHBOARD, ICON_DASHBOARD, TITLE_DASHBOARD));
-			if (SecurityUtils.isAccessGranted(UsersView.class)) {
-				pages.add(new PageInfo(PAGE_USERS, ICON_USERS, TITLE_USERS));
-			}
-			if (SecurityUtils.isAccessGranted(ProductsView.class)) {
-				pages.add(
-					new PageInfo(PAGE_PRODUCTS, ICON_PRODUCTS, TITLE_PRODUCTS));
-			}
-			pages.add(new PageInfo(PAGE_LOGOUT, ICON_LOGOUT, TITLE_LOGOUT));
-		}
-		appNavigation.init(pages, PAGE_DEFAULT, PAGE_LOGOUT);
-
 		getElement().appendChild(confirmDialog.getElement());
 		getElement().appendChild(new BakeryCookieConsent().getElement());
+		getElement().addAttachListener(e -> UI.getCurrent().add(new OfflineBanner()));
+	}
+
+	@Override
+	protected void configure(AppLayout appLayout) {
+		appLayout.setBranding(new Span("###Bakery###").getElement());
+
+		if (SecurityUtils.isUserLoggedIn()) {
+			setMenuItem(appLayout, new RoutingMenuItem(VaadinIcon.EDIT.create(), TITLE_STOREFRONT, PAGE_STOREFRONT));
+			setMenuItem(appLayout, new RoutingMenuItem(VaadinIcon.CLOCK.create(), TITLE_DASHBOARD, PAGE_DASHBOARD));
+
+			if (SecurityUtils.isAccessGranted(UsersView.class)) {
+				setMenuItem(appLayout, new RoutingMenuItem(VaadinIcon.USER.create(), TITLE_USERS, PAGE_USERS));
+			}
+			if (SecurityUtils.isAccessGranted(ProductsView.class)) {
+				setMenuItem(appLayout, new RoutingMenuItem(VaadinIcon.CALENDAR.create(), TITLE_PRODUCTS, PAGE_PRODUCTS));
+			}
+
+			setMenuItem(appLayout, new ActionMenuItem(VaadinIcon.ARROW_RIGHT.create(), TITLE_LOGOUT, e ->
+					UI.getCurrent().getPage().executeJavaScript("location.assign('logout')")));
+		}
+		getElement().addEventListener("search-focus", e -> {
+			appLayout.getElement().getClassList().add("hide-navbar");
+		});
+
+		getElement().addEventListener("search-blur", e -> {
+			appLayout.getElement().getClassList().remove("hide-navbar");
+		});
+	}
+
+	private void setMenuItem(AppLayout appLayout, MenuItem menuItem) {
+		menuItem.getElement().setAttribute("theme", "icon-on-top");
+		appLayout.addMenuItem(menuItem);
 	}
 
 	@Override
@@ -108,9 +93,7 @@ public class MainView extends PolymerTemplate<TemplateModel>
 
 	@Override
 	public void showRouterLayoutContent(HasElement content) {
-		if (content != null) {
-			getElement().appendChild(content.getElement());
-		}
+		super.showRouterLayoutContent(content);
 
 		this.confirmDialog.setOpened(false);
 		if (content instanceof HasConfirmation) {
