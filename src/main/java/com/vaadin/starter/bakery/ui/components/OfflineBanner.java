979bf6271f0e1f6641b2b02352ec26ea825a7ef5
alexberazouski <alex@vaadin.com>
2018-10-16 17:46:59 +0300

    Use app layout (#658)

---
+++ src/main/java/com/vaadin/starter/bakery/ui/components/OfflineBanner.java
@@ -0,0 +1,11 @@
+package com.vaadin.starter.bakery.ui.components;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.HtmlImport;
+
+@Tag("offline-banner")
+@HtmlImport("src/components/offline-banner.html")
+public class OfflineBanner extends Component {
+
+}
