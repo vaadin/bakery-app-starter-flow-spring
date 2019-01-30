f7d84dc5d3f2134e1667a54ee51f4dec17b1d239
Tulio Garcia <tulio@vaadin.com>
2019-01-30 13:41:40 +0200

    Use login for flow component (#716)
    * Added iron-ajax because It is used in offline page

--- src/main/java/com/vaadin/starter/bakery/ui/views/login/LoginView.java
+++ src/main/java/com/vaadin/starter/bakery/ui/views/login/LoginView.java
@@ -1,50 +1,71 @@
 package com.vaadin.starter.bakery.ui.views.login;
 
-import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.login.LoginI18n;
+import com.vaadin.flow.component.login.LoginOverlay;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.Viewport;
-import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 import com.vaadin.flow.router.AfterNavigationEvent;
 import com.vaadin.flow.router.AfterNavigationObserver;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.InitialPageSettings;
-import com.vaadin.flow.server.PageConfigurator;
 import com.vaadin.flow.templatemodel.TemplateModel;
+import com.vaadin.starter.bakery.app.security.SecurityUtils;
 import com.vaadin.starter.bakery.ui.components.BakeryCookieConsent;
 import com.vaadin.starter.bakery.ui.utils.BakeryConst;
+import com.vaadin.starter.bakery.ui.views.storefront.StorefrontView;
 
-@Tag("login-view")
-@HtmlImport("src/views/login/login-view.html")
-
-@Route(value = "login")
+@Route
 @PageTitle("###Bakery###")
-
+@HtmlImport("styles/shared-styles.html")
 @Viewport(BakeryConst.VIEWPORT)
-public class LoginView extends PolymerTemplate<LoginView.Model> implements PageConfigurator, AfterNavigationObserver {
+public class LoginView extends VerticalLayout
+	implements AfterNavigationObserver, BeforeEnterObserver {
 
-	
+	private LoginOverlay login = new LoginOverlay();
+
 	public LoginView() {
-		getElement().appendChild(new BakeryCookieConsent().getElement());
+		getElement().appendChild(
+			new BakeryCookieConsent().getElement(), login.getElement());
+
+		LoginI18n i18n = LoginI18n.createDefault();
+		i18n.setHeader(new LoginI18n.Header());
+		i18n.getHeader().setTitle("###Bakery###");
+		i18n.getHeader().setDescription(
+			"admin@vaadin.com + admin\n" + "barista@vaadin.com + barista");
+		i18n.setAdditionalInformation(null);
+		i18n.setForm(new LoginI18n.Form());
+		i18n.getForm().setSubmit("Sign in");
+		i18n.getForm().setTitle("Sign in");
+		i18n.getForm().setUsername("Email");
+		i18n.getForm().setPassword("Password");
+		login.setI18n(i18n);
+		login.getElement().setAttribute("no-forgot-password", true);
+		login.setAction("login");
+		login.setOpened(true);
 	}
 
 	@Override
-	public void configurePage(InitialPageSettings settings) {
-		// Force login page to use Shady DOM to avoid problems with browsers and
-		// password managers not supporting shadow DOM
-		settings.addInlineWithContents(InitialPageSettings.Position.PREPEND,
-				"if(window.customElements) {" +
-						"window.customElements.forcePolyfill=true;" +
-						"window.ShadyDOM={force:true}};", InitialPageSettings.WrapMode.JAVASCRIPT);
+	public void beforeEnter(BeforeEnterEvent event) {
+		if (SecurityUtils.isUserLoggedIn()) {
+			// Needed manually to change the URL because of https://github.com/vaadin/flow/issues/4189
+			UI.getCurrent().getPage().getHistory().replaceState(null, "");
+			event.rerouteTo(StorefrontView.class);
+		}
 	}
 
 	@Override
 	public void afterNavigation(AfterNavigationEvent event) {
-		boolean error = event.getLocation().getQueryParameters().getParameters().containsKey("error");
-		getModel().setError(error);
+		login.setError(
+			event.getLocation().getQueryParameters().getParameters().containsKey(
+				"error"));
 	}
 
 	public interface Model extends TemplateModel {
+
 		void setError(boolean error);
 	}
 
