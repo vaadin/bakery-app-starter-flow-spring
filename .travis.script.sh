d105e4448eaf06dbeb23f5ae1f4bdd1b61038b52
Guille <alvarezguille@users.noreply.github.com>
2019-10-07 11:31:07 +0300

    Tests related cherry-picks (#954)
    Cherry picks
    * Change dist to xenial (#935)
    
    Google has dropped Trusty support for Chrome
    Change to openjdk8 as oraclejdk8 is not available
    Update webdrivers
    
    * Update com.saucelabs.maven.plugin to 2.1.25 (#941)

--- .travis.script.sh
+++ .travis.script.sh
@@ -7,4 +7,4 @@
 
 TESTBENCH=-Dvaadin.proKey=$VAADIN_PRO_KEY
 
-mvn -B -e -V -Pit clean verify -DrunLint -Dvaadin.productionMode $TESTBENCH
+xvfb-run  --server-args="-screen 0 1024x768x24" mvn -B -e -V -Pit clean verify -DrunLint -Dvaadin.productionMode $TESTBENCH
