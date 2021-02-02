49398f10982b9fea6547776f6deb4bb223315149
Mikhail Shabarov <mikhail@vaadin.com>
2021-02-02 16:09:08 +0200

    Apply url parameters template feature

--- src/main/java/com/vaadin/starter/bakery/ui/views/storefront/OrderPresenter.java
+++ src/main/java/com/vaadin/starter/bakery/ui/views/storefront/OrderPresenter.java
@@ -76,7 +76,7 @@ public class OrderPresenter {
 	}
 
 	void cancel() {
-		entityPresenter.cancel(() -> close(), () -> view.setOpened(true));
+		entityPresenter.cancel(this::close, () -> view.setOpened(true));
 	}
 
 	void closeSilently() {
@@ -85,7 +85,10 @@ public class OrderPresenter {
 	}
 
 	void edit() {
-		UI.getCurrent().navigate(BakeryConst.PAGE_STOREFRONT_EDIT + "/" + entityPresenter.getEntity().getId());
+        UI.getCurrent()
+                .navigate(BakeryConst.PAGE_STOREFRONT + "/"
+                        + entityPresenter.getEntity().getId() + "/"
+                        + BakeryConst.EDIT_SEGMENT);
 	}
 
 	void back() {
