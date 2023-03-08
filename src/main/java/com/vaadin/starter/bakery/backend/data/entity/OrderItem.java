e849ce9d6e13414342686166ec3bc71995a5ed8c
Manolo Carrasco <manolo@vaadin.com>
2023-03-08 19:34:37 +0100

    chore: update to update-to-v24

--- src/main/java/com/vaadin/starter/bakery/backend/data/entity/OrderItem.java
+++ src/main/java/com/vaadin/starter/bakery/backend/data/entity/OrderItem.java
@@ -1,10 +1,10 @@
 package com.vaadin.starter.bakery.backend.data.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class OrderItem extends AbstractEntity {
