c3c28fa7a0755d062ff74a146ae147076fabe281
Manolo Carrasco <manolo@vaadin.com>
2023-03-08 19:32:53 +0100

    chore: update to update-to-v24

--- src/main/java/com/vaadin/starter/bakery/ui/views/storefront/StorefrontView.java
+++ src/main/java/com/vaadin/starter/bakery/ui/views/storefront/StorefrontView.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-import javax.annotation.security.PermitAll;
+import jakarta.annotation.security.PermitAll;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
