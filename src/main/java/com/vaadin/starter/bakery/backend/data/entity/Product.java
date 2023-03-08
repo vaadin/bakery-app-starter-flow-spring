c4dc0ac71a4643779eb06497e20752d2a25b0f0a
Manolo Carrasco <manolo@vaadin.com>
2023-03-08 19:38:34 +0100

    chore: update to update-to-v24

--- src/main/java/com/vaadin/starter/bakery/backend/data/entity/Product.java
+++ src/main/java/com/vaadin/starter/bakery/backend/data/entity/Product.java
@@ -1,11 +1,11 @@
 package com.vaadin.starter.bakery.backend.data.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import java.util.Objects;
 
 @Entity
