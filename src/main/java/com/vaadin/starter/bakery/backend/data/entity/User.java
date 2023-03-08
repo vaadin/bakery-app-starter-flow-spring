b65f54b7e8f2c3695e3bc4cbbdebde22f6466cac
Manolo Carrasco <manolo@vaadin.com>
2023-03-08 19:33:59 +0100

    chore: update to update-to-v24

--- src/main/java/com/vaadin/starter/bakery/backend/data/entity/User.java
+++ src/main/java/com/vaadin/starter/bakery/backend/data/entity/User.java
@@ -1,14 +1,14 @@
 package com.vaadin.starter.bakery.backend.data.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.Objects;
 
 @Entity(name="UserInfo")
